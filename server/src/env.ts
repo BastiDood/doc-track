10df7a1188ad7bbaefb2312020e3daa01db6a59d
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-09-16 20:36:31 +0800

    feat(server): extract `VAPID_PRV_KEY` from environment

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,4 +3,10 @@ import { assert } from './deps.ts';
 const PORT = Deno.env.get('PORT');
 assert(PORT !== undefined);
 
-export const env = { PORT: parseInt(PORT, 10) };
+const VAPID_PRV_KEY = Deno.env.get('VAPID_PRV_KEY');
+assert(VAPID_PRV_KEY !== undefined);
+
+export const env = {
+    PORT: parseInt(PORT, 10),
+    VAPID_PRV_KEY,
+};
