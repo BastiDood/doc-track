25affd591296c872cdfb33084dcdd7364610a20a
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-01-16 15:58:42 +0800

    deps(server): upgrade dependencies

--- server/src/deps.ts
+++ server/src/deps.ts
@@ -1,4 +1,4 @@
-export { assert } from "https://deno.land/std@0.160.0/testing/asserts.ts?doc=&s=assert";
-export { Status } from 'https://deno.land/std@0.160.0/http/http_status.ts?s=Status';
+export { assert } from "https://deno.land/std@0.172.0/testing/asserts.ts?doc=&s=assert";
+export { Status } from 'https://deno.land/std@0.172.0/http/http_status.ts?s=Status';
 
-export { z } from 'https://deno.land/x/zod@v3.19.1/mod.ts?s=z';
+export { z } from 'https://deno.land/x/zod@v3.20.2/mod.ts?s=z';
