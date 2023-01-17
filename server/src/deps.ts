1da6c0809473366b297fe564dccffc3cf718f845
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-01-17 15:13:18 +0800

    deps(server): update `std` version

--- server/src/deps.ts
+++ server/src/deps.ts
@@ -1,4 +1,4 @@
-export { assert } from "https://deno.land/std@0.172.0/testing/asserts.ts?doc=&s=assert";
-export { Status } from 'https://deno.land/std@0.172.0/http/http_status.ts?s=Status';
+export { assert } from "https://deno.land/std@0.173.0/testing/asserts.ts?doc=&s=assert";
+export { Status } from 'https://deno.land/std@0.173.0/http/http_status.ts?s=Status';
 
 export { z } from 'https://deno.land/x/zod@v3.20.2/mod.ts?s=z';
