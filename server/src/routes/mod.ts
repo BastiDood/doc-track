21e4c1333f5be3ca9331bbb494816358f55a2e21
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-03 19:58:35 +0800

    chore(server): move API handlers to own folder

--- server/src/routes/mod.ts
+++ server/src/routes/mod.ts
@@ -1,14 +1,14 @@
 import { Status } from 'http';
 import { Pool } from 'postgres';
 
-import { handleSubscribe } from './subscribe.ts';
+import { handleSubscribe } from './api/subscribe.ts';
 import { handleCallback, handleLogin } from './auth/mod.ts';
 
 export function get(pool: Pool, req: Request) {
     const { pathname, searchParams } = new URL(req.url);
     switch (pathname) {
-        case '/login': return handleLogin(pool, req);
-        case '/callback': return handleCallback(pool, req, searchParams);
+        case '/auth/login': return handleLogin(pool, req);
+        case '/auth/callback': return handleCallback(pool, req, searchParams);
         default: return new Response(null, { status: Status.NotFound });
     }
 }
@@ -16,7 +16,7 @@ export function get(pool: Pool, req: Request) {
 export function post(pool: Pool, req: Request) {
     const { pathname } = new URL(req.url);
     switch (pathname) {
-        case '/subscribe': return handleSubscribe(pool, req);
+        case '/api/subscribe': return handleSubscribe(pool, req);
         default: return new Response(null, { status: Status.NotFound });
     }
 }
