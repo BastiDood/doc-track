4f4751fcbc50188b4cc5a43c1b10c248698769ec
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-04 23:36:55 +0800

    feat(server): return subscription ID to caller

--- server/src/routes/api/subscribe.ts
+++ server/src/routes/api/subscribe.ts
@@ -8,7 +8,7 @@ export async function handleSubscribe(pool: Pool, req: Request) {
     // FIXME: Add CSRF tokens and nonces to mitigate replay attacks.
     const sub = PushSubscriptionSchema.parse(await req.json());
     const db = await Database.fromPool(pool);
-    await db.pushSubscription(sub);
+    const id = await db.pushSubscription(sub);
     db.release();
-    return new Response(null, { status: Status.Created });
+    return new Response(id.toString(), { status: Status.Created });
 }
