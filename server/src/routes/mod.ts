391e1db542b14523986c3334ca898d738c7aaebd
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-07 18:33:33 +0800

    feat(server): include file server

--- server/src/routes/mod.ts
+++ server/src/routes/mod.ts
@@ -1,5 +1,9 @@
+import { assertInstanceOf } from 'asserts';
+import { contentType } from 'content-type';
 import { Status } from 'http';
+import { error, info } from 'log';
 import { Pool } from 'postgres';
+import { join, extname } from 'posix';
 
 import {
     handleGetAllCategories,
@@ -12,13 +16,36 @@ import { handleRevokeInvitation } from './api/invite.ts';
 import { handleSubscribe } from './api/subscribe.ts';
 import { handleCallback, handleLogin, handleLogout } from './auth/mod.ts';
 
-export function handleGet(pool: Pool, req: Request) {
+export async function handleGet(pool: Pool, req: Request) {
     const { pathname, searchParams } = new URL(req.url);
     switch (pathname) {
+        case '/': {
+            const path = '../client/dist' + join(pathname, 'index.html');
+            const file = await Deno.open(path);
+            return new Response(file.readable);
+        }
         case '/api/categories': return handleGetAllCategories(pool, req);
         case '/auth/login': return handleLogin(pool, req);
         case '/auth/callback': return handleCallback(pool, req, searchParams);
-        default: return new Response(null, { status: Status.NotFound });
+        default: break;
+
+    }
+
+    const mime = contentType(extname(pathname));
+    if (!mime) {
+        error(`[GET] Cannot determine MIME type of ${pathname}`);
+        return new Response(null, { status: Status.NotFound });
+    }
+
+    const path = '../client/dist' + pathname;
+    try {
+        const file = await Deno.open(path);
+        info(`[GET] Read static file ${path}`);
+        return new Response(file.readable, { headers: { 'Content-Type': mime } });
+    } catch (err) {
+        assertInstanceOf(err, Deno.errors.NotFound);
+        error(`[GET] ${pathname} not found`);
+        return new Response(null, { status: Status.NotFound });
     }
 }
 
@@ -28,7 +55,9 @@ export function handlePost(pool: Pool, req: Request) {
         case '/api/category': return handleCreateCategory(pool, req);
         case '/api/subscribe': return handleSubscribe(pool, req);
         case '/api/office': return handleCreateOffice(pool, req);
-        default: return new Response(null, { status: Status.NotFound });
+        default:
+            error(`[POST] ${pathname} not found`);
+            return new Response(null, { status: Status.NotFound });
     }
 }
 
@@ -37,7 +66,9 @@ export function handlePut(pool: Pool, req: Request) {
     switch (pathname) {
         case '/api/category': return handleRenameCategory(pool, req);
         case '/api/office': return handleUpdateOffice(pool, req);
-        default: return new Response(null, { status: Status.NotFound });
+        default:
+            error(`[PUT] ${pathname} not found`);
+            return new Response(null, { status: Status.NotFound });
     }
 }
 
@@ -47,6 +78,8 @@ export function handleDelete(pool: Pool, req: Request) {
         case '/api/category': return handleDeleteCategory(pool, req);
         case '/api/invite': return handleRevokeInvitation(pool, req, searchParams);
         case '/auth/logout': return handleLogout(pool, req);
-        default: return new Response(null, { status: Status.NotFound });
+        default:
+            error(`[DELETE] ${pathname} not found`);
+            return new Response(null, { status: Status.NotFound });
     }
 }
