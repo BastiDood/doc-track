25e43d538ab959e9f4ed2be4787e3726ac89c2de
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-08 17:14:56 +0800

    refactor(client): retrieve VAPID public key from API

--- client/src/pages/register.ts
+++ client/src/pages/register.ts
@@ -4,8 +4,11 @@ async function getSubscription(manager: PushManager): Promise<PushSubscription> 
     const maybeSub = await manager.getSubscription();
     if (maybeSub !== null) return maybeSub;
 
+    const pubResponse = await fetch('/vapid');
+    assert(pubResponse.ok);
+    const applicationServerKey = await pubResponse.arrayBuffer();
     const sub = await manager.subscribe({
-        applicationServerKey: process.env.VAPID_PUB_KEY,
+        applicationServerKey,
         userVisibleOnly: true,
     });
 
@@ -17,7 +20,7 @@ async function getSubscription(manager: PushManager): Promise<PushSubscription> 
     assert(auth);
     assert(p256dh);
 
-    const response = await fetch('/api/subscribe', {
+    const subResponse = await fetch('/api/subscribe', {
         method: 'POST',
         body: JSON.stringify({
             endpoint,
@@ -27,7 +30,7 @@ async function getSubscription(manager: PushManager): Promise<PushSubscription> 
         }),
     });
 
-    if (response.status !== 201)
+    if (subResponse.status !== 201)
         throw new Error('failed to submit subscription');
 
     return sub;
