b1913e20b7536433bc0d1694defdf53d50b0f5be
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-12 14:52:21 +0800

    feat(server): implement handler for setting staff permissions

--- server/src/routes/api/staff.ts
+++ server/src/routes/api/staff.ts
@@ -6,6 +6,70 @@ import { Pool } from 'postgres';
 import { Database } from '../../database.ts';
 
 /**
+ * Sets the new permissions for a staff member.
+ *
+ * # Inputs
+ * - Requires a valid session ID of an office administrator.
+ * - Accepts the office ID of the session via the `office` query parameter.
+ * - Accepts the new permission integer of the staff member via the `perms` query parameter.
+ * - Accepts the user UUID in plaintext via the {@linkcode Request} body.
+ *
+ * # Outputs
+ * - `204` => successfully updated the staff permissions
+ * - `400` => `office` parameter, user UUID, or permissions is unacceptable
+ * - `401` => session ID is absent, expired, or otherwise malformed
+ * - `403` => session has insufficient permissions
+ * - `404` => staff member does not exist
+ */
+export async function handleSetStaffPermissions(pool: Pool, req: Request, params: URLSearchParams) {
+    const { sid } = getCookies(req.headers);
+    if (!sid) {
+        error('[Staff] Absent session ID');
+        return new Response(null, { status: Status.Unauthorized });
+    }
+
+    const office = params.get('office');
+    const oid = office ? parseInt(office, 10) : NaN;
+    if (isNaN(oid)) {
+        error(`[Staff] Session ${sid} provided invalid office ID`);
+        return new Response(null, { status: Status.BadRequest });
+    }
+
+    const perms = params.get('perms');
+    const setPerms = perms ? parseInt(perms, 10) : NaN;
+    if (isNaN(setPerms)) {
+        error(`[Staff] Session ${sid} provided invalid office ID`);
+        return new Response(null, { status: Status.BadRequest });
+    }
+
+    const user = await req.text();
+    if (!user) {
+        error(`[Staff] Session ${sid} provided empty user ID`);
+        return new Response(null, { status: Status.BadRequest });
+    }
+
+    const db = await Database.fromPool(pool);
+    try {
+        const permissions = await db.getPermissionsFromSession(sid, oid);
+        if (permissions === null) {
+            error(`[Staff] Invalid session ${sid}`);
+            return new Response(null, { status: Status.Unauthorized });
+        }
+
+        // TODO: Check permissions
+        if (await db.setStaffPermissions(user, oid, setPerms)) {
+            info(`[Category] Session ${sid} set the staff permissions of user ${user} in office ${oid} as ${setPerms}`);
+            return new Response(null, { status: Status.NoContent });
+        }
+
+        error(`[Staff] Session ${sid} attempted to set the staff permissions of non-existent user ${user} in office ${oid} as ${setPerms}`);
+        return new Response(null, { status: Status.NotFound });
+    } finally {
+        db.release();
+    }
+}
+
+/**
  * Retires an existing member. Deletes it if no one references the staff member.
  *
  * # Inputs
