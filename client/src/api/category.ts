ca88c8ae8fec39688eb0f3dea0eb58b4162e9a9e
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-18 17:58:23 +0800

    fix(client/api): refer to project `model`

--- client/src/api/category.ts
+++ client/src/api/category.ts
@@ -8,35 +8,28 @@ import {
     UNAUTHORIZED,
     FORBIDDEN
 } from 'http-status';
-import { z } from 'zod';
+
+import { type Category as CategoryType, CategorySchema } from '~model/category.ts';
 
 import {
     InsufficientPermissions,
     InvalidInput,
     InvalidSession,
     UnexpectedStatusCode
-} from './error';
+} from './error.ts';
 
 export namespace Category {
-    export const Schema = z.object({
-        id: z.number().int().positive(),
-        name: z.string().min(1).max(20),
-        active: z.boolean(),
-    });
-
-    export type Category = z.infer<typeof Schema>;
-
     /**
      * Requires a valid session for a valid session.
      * @returns list of active {@linkcode Category} entries
      */
-    export async function getAllActive(): Promise<Category[]> {
+    export async function getAllActive(): Promise<CategoryType[]> {
         const res = await fetch('/api/categories', {
             credentials: 'same-origin',
             headers: { 'Accept': 'application/json' },
         });
         switch (res.status) {
-            case OK: return Schema.array().parse(await res.json());
+            case OK: return CategorySchema.array().parse(await res.json());
             case UNAUTHORIZED: throw new InvalidSession;
             default: throw new UnexpectedStatusCode;
         }
@@ -46,7 +39,7 @@ export namespace Category {
      * Requires a valid session for a system operator.
      * @returns ID of the new {@linkcode Category} entry
      */
-    export async function create(name: Category['name']): Promise<Category['id']> {
+    export async function create(name: CategoryType['name']): Promise<CategoryType['id']> {
         const res = await fetch('/api/category', {
             credentials: 'same-origin',
             method: 'POST',
@@ -57,7 +50,7 @@ export namespace Category {
             },
         });
         switch (res.status) {
-            case CREATED: return Schema.shape.id.parse(JSON.parse(await res.json()));
+            case CREATED: return CategorySchema.shape.id.parse(JSON.parse(await res.json()));
             case BAD_REQUEST: throw new InvalidInput;
             case UNAUTHORIZED: throw new InvalidSession;
             case FORBIDDEN: throw new InsufficientPermissions;
@@ -69,7 +62,7 @@ export namespace Category {
      * Requires a valid session for a system operator.
      * @returns `false` if {@linkcode Category} ID does not exist
      */
-    export async function rename(id: Category['id'], name: Category['name']): Promise<boolean> {
+    export async function rename(id: CategoryType['id'], name: CategoryType['name']): Promise<boolean> {
         const res = await fetch('/api/category', {
             credentials: 'same-origin',
             method: 'PUT',
@@ -95,7 +88,7 @@ export namespace Category {
      * @returns `true` if {@linkcode Category} was successfully deleted
      * @returns `null` if {@linkcode Category} ID does not exist
      */
-    export async function remove(id: Category['id']): Promise<boolean | null> {
+    export async function remove(id: CategoryType['id']): Promise<boolean | null> {
         const res = await fetch(`/api/category?id=${id}`, {
             method: 'DELETE',
             credentials: 'same-origin',
@@ -116,7 +109,7 @@ export namespace Category {
      * @returns string containing the {@linkcode Category} name if successful
      * @returns `null` if {@linkcode Category} ID does not exist
      */
-    export async function activate(id: Category['id']): Promise<Category['name'] | null> {
+    export async function activate(id: CategoryType['id']): Promise<CategoryType['name'] | null> {
         const res = await fetch(`/api/category?id=${id}`, {
             method: 'PATCH',
             credentials: 'same-origin',
