87af58a30c8a1c63f5e27267c6e7fabe54e41b38
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-25 17:25:48 +0800

    feat(client/api): implement wrapper for snapshot insertion

---
+++ client/src/api/snapshot.ts
@@ -0,0 +1,46 @@
+import {
+    CREATED,
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    NOT_ACCEPTABLE,
+    CONFLICT,
+} from 'http-status';
+
+import type { Office } from '~model/office.ts';
+
+import { type InsertSnapshotError, InsertSnapshotErrorSchema } from '~model/api.ts';
+import { type Snapshot as SnapshotType, SnapshotSchema } from '~model/snapshot.ts';
+
+import {
+    InsufficientPermissions,
+    InvalidInput,
+    InvalidSession,
+    BadContentNegotiation,
+    UnexpectedStatusCode
+} from './error.ts';
+
+export namespace Snapshot {
+    export async function insert(
+        oid: Office['id'],
+        info: Omit<SnapshotType, 'creation' | 'evaluator'>,
+    ): Promise<SnapshotType['creation'] | InsertSnapshotError> {
+        const res = await fetch(`/api/snapshot?office=${oid}`, {
+            method: 'POST',
+            body: JSON.stringify(info),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        switch (res.status) {
+            case CREATED: return SnapshotSchema.shape.creation.parse(await res.json());
+            case CONFLICT: return InsertSnapshotErrorSchema.parse(await res.json());
+            case BAD_REQUEST: throw new InvalidInput;
+            case UNAUTHORIZED: throw new InvalidSession;
+            case FORBIDDEN: throw new InsufficientPermissions;
+            case NOT_ACCEPTABLE: throw new BadContentNegotiation;
+            default: throw new UnexpectedStatusCode;
+        }
+    }
+}
