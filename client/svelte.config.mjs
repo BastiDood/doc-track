d5cb85b7aa2cb68bc0d9966dc3daa196373d14c5
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-02-28 23:34:26 +0800

    fix(client): revert plugins and configuration

--- client/svelte.config.mjs
+++
@@ -1,9 +0,0 @@
-import sveltePreprocess from 'svelte-preprocess';
-import importAssets from 'svelte-preprocess-import-assets';
-
-export default {
-    preprocess: [
-        sveltePreprocess(),
-        importAssets(),
-    ],
-};
