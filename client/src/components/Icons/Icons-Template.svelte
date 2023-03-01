a935c7e72e6816253f016ed010094f2695748fb4
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-01 11:00:11 +0800

    feat: added Icons template and relevant stylesheets

---
+++ client/src/components/Icons/Icons-Template.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "./icon-styles.css"
+    import { IconColor} from "../types.ts";
+    
+    export let name: string;
+    export let color: IconColor = IconColor.Default;
+    export let width: string = "1rem";
+    export let height: string = "1rem";
+
+    const iconUrl = new URL("../../assets/document-blank.svg", import.meta.url);
+
+</script>
+
+<img class="{color}" {width} {height} src="{iconUrl.pathname}" alt="{name}"/>
