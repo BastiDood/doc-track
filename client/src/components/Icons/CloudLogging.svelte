ef2947d3d03cd7be87e983ea83569c1060c3ab08
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-02 23:08:23 +0800

    feat: second tranche of icons

---
+++ client/src/components/Icons/CloudLogging.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "./icon-styles.css"
+    import { IconColor} from "../types.ts";
+    
+    export let color: IconColor = IconColor.Default;
+    export let width: string = "1rem";
+    export let alt:string = "";
+    export let height: string = "1rem";
+
+    /**Replace the next lines to generate an icon.*/
+	const iconUrl = new URL("../../assets/cloud-logging.svg", import.meta.url);
+</script>
+
+<img class="{color}" {width} {height} {alt} src="{iconUrl.pathname}" on:click on:keydown/>
