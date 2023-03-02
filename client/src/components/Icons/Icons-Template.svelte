1582b055a67063fbd38a138ff30780c5cc74b430
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-02 22:05:04 +0800

    feat: added alt text and event listeners to icons

--- client/src/components/Icons/Icons-Template.svelte
+++ client/src/components/Icons/Icons-Template.svelte
@@ -4,10 +4,11 @@
     
     export let color: IconColor = IconColor.Default;
     export let width: string = "1rem";
+    export let alt:string = "";
     export let height: string = "1rem";
 
     const iconUrl = new URL("../../assets/document-blank.svg", import.meta.url);
 
 </script>
 
-<img class="{color}" {width} {height} src="{iconUrl.pathname}" alt="{name}"/>
+<img class="{color}" {width} {height} {alt} src="{iconUrl.pathname}" on:click on:keydown/>
