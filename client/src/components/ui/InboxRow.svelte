3495f5c95194e74712e6a2d8d55009395bfd7977
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-07 17:14:24 +0800

    feat: added InboxRow.svelte

---
+++ client/src/components/ui/InboxRow.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import RowTemplate from "./RowTemplate.svelte";
+    import DocumentImport from "../Icons/DocumentImport.svelte";
+    import Checkmark from "../Icons/Checkmark.svelte";
+    import Close from "../Icons/Close.svelte";
+    import Camera from "../Icons/Camera.svelte";
+
+    import { createEventDispatcher } from 'svelte';
+
+    export let id: string;
+    export let category: number;
+    export let title: string;
+
+    const dispatch = createEventDispatcher();
+
+</script>
+
+<RowTemplate
+    on:overflowclick = {() => dispatch('overflowclick', {id})}
+    on:click = {(e) => console.log(e)}
+>
+    <DocumentImport slot="displayIcon"/>
+    {title} ID: {id} Category: {category}
+    
+    <div slot="actionIcons">
+        <Checkmark
+        on:click = {() => dispatch('acceptDocument', {id})} />
+        <Close
+        on:click = {() => dispatch('declineDocument', {id})} />
+        <Camera
+        on:click = {() => dispatch('toggleCamera', {id})}/>
+
+    </div>
+
+</RowTemplate>
