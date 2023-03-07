ece2f6e52ccd792006e0a2f689da413e5d26072f
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-03-07 20:20:38 +0800

    feat(client): add dashboard boilerplate

--- client/src/pages/dashboard/Dashboard.svelte
+++ client/src/pages/dashboard/Dashboard.svelte
@@ -1,1 +1,16 @@
-<main>Hello World</main>
+<script lang="ts">
+    import { register } from '../register.ts';
+    import { Session } from '../../api/session.ts';
+    const ready = register();
+</script>
+<main>
+    {#await ready}
+        Waiting for service worker...
+    {:then}
+        {#await Session.getUser()}
+            Getting user...
+        {:then user}
+            Hello, {user.name}!
+        {/await}
+    {/await}
+</main>
