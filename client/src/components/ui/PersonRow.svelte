807f32bbfa7a677e2f9e48172756dcc1639208d5
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-07 17:59:56 +0800

    feat: added PersonRow.svelte

---
+++ client/src/components/ui/PersonRow.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import RowTemplate from "./RowTemplate.svelte";
+    import Search from "../icons/Search.svelte";
+    import Edit from "../icons/Edit.svelte";
+    import Close from "../icons/Close.svelte";
+
+    import { createEventDispatcher } from 'svelte';
+    import { IconSize } from '../types.ts';
+
+    export let pictureSize = IconSize.Normal;
+    // From user.ts
+    export let id: string;
+    export let name: string;
+    export let email: string;
+    export let picture: string;
+    export let global_permission: number;
+
+    // From staff.ts
+    export let office: number;
+    export let local_permission: number;
+    export let active: boolean;
+    
+    const dispatch = createEventDispatcher();
+
+</script>
+
+<RowTemplate
+    on:overflowclick = {() => dispatch('overflowclick', {id})}
+    on:click = {(e) => console.log(e)}
+>
+    <img width={pictureSize} height={pictureSize} src={picture} alt={name} slot="displayIcon">
+    
+    {name} ID: {id} Email: {email} Office: {office} Global Perms: {global_permission} Local Perms: {local_permission}
+    <div slot="actionIcons">
+        <Search
+            on:click = {() => dispatch('showUserInfo', {id})} />
+        <Edit 
+            on:click = {() => dispatch('editUser', {id})} />
+        <Close 
+            on:click = {() => dispatch('removeUser', {id})} />
+    </div>
+
+</RowTemplate>
+
+<style>
+    img {
+        border-radius: 50%;
+    }
+</style>
