850b0d7cd8c84ccde5842e27574689c252f86978
Arukuen <adsarmiento3@up.edu.ph>
2023-03-08 18:41:13 +0800

    feat(client): added textinput component

---
+++ client/src/components/ui/TextInput.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { InputType } from '../types.ts';
+  export let type: InputType = InputType.Primary;
+  export let placeholder = '';
+  export let name: string;
+  export let isRoundedBorder = true;
+</script>
+
+<label for={name}><slot></slot></label>
+<input type={type} placeholder={placeholder} id={name} class='{isRoundedBorder ? 'round-border' : ''}'/>
+
+<style>
+  input {
+    border: #4040f2 2px solid;
+    padding: 5px 8px;
+  }
+
+  .round-border {
+    border-radius: 15px;
+  }
+</style>
