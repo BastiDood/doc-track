c2d8cff6f0b45eddd6f8b920b277651d854f5aee
Justin Ruaya <jrruaya1@up.edu.ph>
2023-03-15 10:18:00 +0800

    feat(client): show navbar buttons; elements are using CSS flexbox

--- client/src/components/ui/Navbar.svelte
+++ client/src/components/ui/Navbar.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
     import { Session } from '../../api/session.ts';
+    import { onMount } from "svelte";
 
     import Button from '../../components/ui/Button.svelte';
     import Logout from '../../components/icons/Logout.svelte';
 
     import { ButtonType } from '../../components/types.ts';
+
+    let mobile = false;
+
+    // TODO: Updates based on permissions
+    const navItems = [
+        { label: "logo", href: "#" },
+        { label: "Item 1", href: "#" },
+        { label: "Item 2", href: "#" },
+        { label: "Item 3", href: "#" },
+        { label: "Item 4", href: "#" },
+        { label: "Item 5", href: "#" },
+        { label: "Item 6", href: "#" },
+        { label: "Item 7", href: "#" }
+    ];
 </script>
 
 <nav>
@@ -15,11 +30,15 @@
             Hello, {user.name}!
         {/await}
     </p>
-    <div>
-        <form method="POST" action="/auth/logout">
-            <input type="submit" value="Logout" />
-        </form>
-    </div>
+        <ul class="navelements">
+            {#each navItems as item}
+                <li><a href={item.href} class="navitem">item.label</a></li>
+            {/each}
+            <li><form method="POST" action="/auth/logout" class="navitem">
+                    <input type="submit" value="Logout" />
+                </form>
+            </li>
+        </ul>
 </nav>
 
 <style>
@@ -27,9 +46,29 @@
         display: flex;
         background-color: blueviolet;
         justify-content: space-between;
+        align-items: flex-start;
+        box-shadow: 0 1px 8px #ddd;
         padding: 0.5rem;
         position: sticky;
     }
+
+    .navelements {
+        display: flex;
+        justify-content: flex-end;
+        list-style-type: none;
+        align-items: stretch;
+    }
+    
+    .navitem {
+        list-style: none;
+        padding: 1em;
+        display: inline-block;
+    }
+
+    .navitem:hover {
+        background-color: blue;
+    }
+    
 
     p {
         margin: 0;
