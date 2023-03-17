e0d23b6b5a3398a9ac13f66024f94bf8e6916e12
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-17 11:18:36 +0800

    feat(client): added ContextMenu element component

---
+++ client/src/components/ui/contextmenu/ContextElement.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    import { IconSize } from '../../types.ts'
+    import Add from '../../icons/Add.svelte';
+
+    export let iconSize = IconSize.Normal();
+    export let contextData
+    const dispatch = createEventDispatcher();
+
+</script>
+
+<div 
+    on:click={() => dispatch('contextEvent')}
+    on:keydown={() => dispatch('contextEvent')}
+    >
+    <div>
+        <slot name="contextIcon">
+            <Add size={iconSize} />
+        </slot>
+    </div>
+
+    <div>
+        <slot>
+            <p>
+                Default text
+            </p>
+        </slot>
+    </div>
+</div>
+
+<style>
+    @import url('../../../pages/global.css');
+</style>
