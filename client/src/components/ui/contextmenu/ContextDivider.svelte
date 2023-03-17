c2a6846ed7fc7c4b4f41b45d82b49fac2c4f6622
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-17 11:18:20 +0800

    feat(client): added ContextMenu divider component

---
+++ client/src/components/ui/contextmenu/ContextDivider.svelte
@@ -0,0 +1,12 @@
+<hr>
+
+<style>
+    @import url(../../../pages/vars.css);
+
+    hr {
+        color: gray;
+        background-color: gray;
+        width: 0;
+        height: var(--spacing-tiny)
+    }
+</style>
