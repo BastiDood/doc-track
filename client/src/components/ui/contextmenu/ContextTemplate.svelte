bdc3ea7ecc98b1e2f314336e79717b7eae0006c0
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-17 11:18:56 +0800

    feat(client): added ContextMenu template

---
+++ client/src/components/ui/contextmenu/ContextTemplate.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let show = false;
+</script>
+
+<div class="drawer" class:hidden={!show}>
+
+</div>
+
+<style>
+    @import url('../../../pages/vars.css');
+
+
+    div {
+        width: 100vw;
+        border: var(--spacing-tiny) solid black;
+        transition: bottom var(--animation-length);
+        bottom: 0;
+        border-radius: var(--border-radius) var(--border-radius);
+    }
+    .hidden {
+        bottom: -100%;
+    }
+
+</style>
+
+
+
