a52e211154fea1e465161d29b957c77483cfd432
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2023-03-17 12:00:37 +0800

    fmt(client): reorder imports

--- client/src/pages/dashboard/Dashboard.svelte
+++ client/src/pages/dashboard/Dashboard.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+    import Router, { push, pop, replace } from 'svelte-spa-router';
+
     import { register } from '../register.ts';
     import { Session } from '../../api/session.ts';
     import Navbar from '../../components/ui/Navbar.svelte';
     import Button from '../../components/ui/Button.svelte'
 
-    import Router from 'svelte-spa-router';
-    import {push, pop, replace} from 'svelte-spa-router';
     import routes from './routes.ts';
 </script>
 <main>
