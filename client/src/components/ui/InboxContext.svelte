c7ee792a2d5d24a977787b2c2ea075e16669493a
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-18 08:40:06 +0800

    feat(client): added container for inbox

---
+++ client/src/components/ui/InboxContext.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    import { Events, ContextPayload, IconSize } from '../types.ts';
+    import ContextTemplate from './contextmenu/ContextTemplate.svelte';
+    import ContextElement from './contextmenu/ContextElement.svelte';
+    import ContextDivider from './contextmenu/ContextDivider.svelte';
+
+    import SendAlt from '../icons/SendAlt.svelte';
+    import CheckboxIndeterminateFilled from '../icons/CheckboxIndeterminateFilled.svelte';
+
+
+    const dispatch = createEventDispatcher();
+    export let show = false;
+    export let payload: ContextPayload;
+    export let iconSize = IconSize.Normal
+</script>
+
+<ContextTemplate bind:show={show}>
+    <ContextElement on:click = {() => dispatch(Events.SendDocument, payload)}>
+        <div slot="contextIcon">
+            <SendAlt size={iconSize}/>
+            Send Document
+        </div>
+    </ContextElement>
+    <ContextDivider/>
+    <ContextElement on:click = {() => dispatch(Events.TerminateDocument, payload)}>
+        <div slot="contextIcon">
+            <CheckboxIndeterminateFilled size={iconSize}/>
+            Terminate Document
+        </div>
+    </ContextElement>
+</ContextTemplate>
