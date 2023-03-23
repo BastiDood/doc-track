aefd0ec13f8c34f8bc404999ae9b5477a83b4de3
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-23 15:19:21 +0800

    fix(client): adjusted renamed routes

--- client/src/pages/dashboard/routes.ts
+++ client/src/pages/dashboard/routes.ts
@@ -1,10 +1,10 @@
 import Barcodes from './views/Barcodes.svelte';
 import Drafts from './views/Drafts.svelte';
 import Inbox from './views/Inbox.svelte';
-import ManageAdministrators from './views/ManageAdministrators.svelte';
-import ManageGlobalSettings from './views/ManageGlobalSettings.svelte';
-import ManageInvites from './views/ManageInvites.svelte';
-import ManageStaff from './views/ManageStaff.svelte';
+import ManageAdministrators from './views/Admins.svelte';
+import ManageGlobalSettings from './views/Settings.svelte';
+import ManageInvites from './views/Invites.svelte';
+import ManageStaff from './views/Staff.svelte';
 import Metrics from './views/Metrics.svelte';
 import Outbox from './views/Outbox.svelte';
 
