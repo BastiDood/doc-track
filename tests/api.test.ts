142c662211b04e14f2a5b70de2cd56500871f7a3
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-03-25 21:08:02 +0800

    test: assert VAPID public key

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,28 +1,52 @@
+import { assert, assertNotStrictEquals } from 'https://deno.land/std@0.160.0/testing/asserts.ts';
+import { equals as bytewiseEquals } from 'bytes';
 import { getSetCookies } from 'cookie';
 import { Pool } from 'postgres';
 
-import { env } from '~server/env.ts';
-import { handleRequest } from '~server/routes/mod.ts'
+import { Batch } from '~client/api/batch.ts';
+import { Category } from '~client/api/category.ts';
+import { Document } from '~client/api/document.ts';
+import { Invite } from '~client/api/invite.ts';
+import { Metrics } from '~client/api/metrics.ts';
+import { Office } from '~client/api/office.ts';
+import { Session } from '~client/api/session.ts';
+import { Snapshot } from '~client/api/snapshot.ts';
+import { Staff } from '~client/api/staff.ts';
+import { User } from '~client/api/user.ts';
+import { Vapid } from '~client/api/vapid.ts';
 
-Deno.test('full API integration test', () => {
+import { env } from '~server/env.ts';
+import { handleRequest } from '~server/routes/mod.ts';
+
+Deno.test('full API integration test', async t => {
     const pool = new Pool({
         user: env.PG_USER,
         password: env.PG_PASSWORD,
         hostname: env.PG_HOSTNAME,
-        port: env.PORT,
+        port: env.PG_PORT,
         database: env.PG_DATABASE,
     }, 1, false);
+    
+    // Preconnect with the database (to mitigate resource leaks)
+    const client = await pool.connect();
+    client.release();
 
     // Mock the `fetch` function and its cookie store
     const origFetch = globalThis.fetch;
     const cookies = new Map<string, string>;
-    globalThis.fetch = async (input: URL | RequestInfo, init?: RequestInit) => {
-        const res = await handleRequest(pool, new Request(input, init));
+    globalThis.fetch = async (input, init) => {
+        const res = await handleRequest(pool, new Request('https://doctrack.app' + input, init));
         for (const { name, value } of getSetCookies(res.headers))
             cookies.set(name, value);
         return res;
     };
 
+    await t.step('VAPID wrappers', async () => {
+        const key = new Uint8Array(await Vapid.getVapidPublicKey());
+        assert(bytewiseEquals(key, env.VAPID_PUB_KEY));
+    });
+
     // Restore the original fetch
     globalThis.fetch = origFetch;
+    await pool.end();
 });
