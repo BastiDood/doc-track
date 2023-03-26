14039ec1b762da2fbdcbde7c0b8900a6c914cbce
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-03-26 16:29:36 +0800

    refactor(client)!: remove action icons from the row template

--- client/src/components/ui/RowTemplate.svelte
+++ client/src/components/ui/RowTemplate.svelte
@@ -1,53 +1,39 @@
 <script lang="ts">
-    import './row-element.css';
     import { createEventDispatcher } from 'svelte';
 
-    import { IconSize } from '../types.ts'
-    import DocumentBlank from '../icons/DocumentBlank.svelte';
+    import { Events, IconSize } from '../types.ts';
+
     import OverflowMenuVertical from '../icons/OverflowMenuVertical.svelte';
 
     export let iconSize = IconSize.Normal;
+    export let title: string;
+
     const dispatch = createEventDispatcher();
-    let showActionIcons = false;
-
-    function toggleOn() {
-        showActionIcons = true;
-    }
-
-    function toggleOff() {
-        showActionIcons = false;
-    }
 </script>
 
-<div 
-    class="parentContainer" 
-    on:mouseenter={toggleOn} 
-    on:focus={toggleOn} 
-    on:mouseleave={toggleOff}
->
-    <div
-        class="itemIcon"     
-        on:click
-        on:keydown
-    >
-        <slot name="displayIcon">
-            <DocumentBlank size={iconSize} />
-        </slot>
-    </div>
-    <div
-        class="itemText"
-        on:click
-        on:keydown
-    >
-        <slot>
-            Default slot Text.
-        </slot>
-    </div>
-    <div class="actionIcon">
-        {#if showActionIcons}
-            <slot name="actionIcons">
-            </slot>
-        {/if}
-        <OverflowMenuVertical size={iconSize} on:click={() => dispatch('overflowclick')} />
+<div id="parent">
+    <div class="icon"><slot name="icon" /></div>
+    <p>{title}</p>
+    <div class="icon" on:keydown on:click|self|stopPropagation={() => dispatch(Events.OverflowClick)}>
+        <OverflowMenuVertical size={iconSize} />
     </div>
 </div>
+
+<style>
+    @import url('../../pages/vars.css');
+
+    #parent {
+        display: flex;
+        border-style: solid;
+        padding: var(--spacing-small);
+    }
+
+    .icon {
+        flex-grow: 0;
+    }
+
+    p {
+        flex-grow: 1;
+        margin: 0;
+    }
+</style>
