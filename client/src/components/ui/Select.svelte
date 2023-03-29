ccac416b0d937b76cfa3ac3b21c477aabc179310
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-29 19:35:15 +0800

    feat(client): added Select component styling

--- client/src/components/ui/Select.svelte
+++ client/src/components/ui/Select.svelte
@@ -11,3 +11,24 @@
         <option value={i}> {options[i]}</option>
     {/each}
 </select>
+
+<style>
+    @import url('../../pages/vars.css');
+
+    select {
+        padding: var(--spacing-small) var(--spacing-normal)  var(--spacing-small) var(--spacing-normal);
+        border: var(--spacing-tiny) solid var(--shadow-color);
+        border-radius: var(--border-radius);
+        background: var(--dashboard-bg);
+        box-shadow: 0 1px 3px -2px #9098A9;
+        cursor: pointer;
+        font-size: var(--normal);
+        transition: all var(--animation-length) ease;
+    }
+
+    select:focus {
+        outline: none;
+        border-color: var(--primary-color);
+        box-shadow: 0 0 0 var(--spacing-tiny) rgba(var(--primary-color), 0.2);
+    }
+</style>
