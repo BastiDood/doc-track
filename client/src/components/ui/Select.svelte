16f59375f197d91f354ef4db6c86abd4fbe93d09
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-03-29 11:41:30 +0800

    feat(client): added select component

---
+++ client/src/components/ui/Select.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let options: Array<string> = [];
+    export let index = 0;
+    export let value;
+
+    $: value = options[index];
+</script>
+
+<select bind:value={index}>
+    {#each options as option, i}
+        <option value={i}> {options[i]}</option>
+    {/each}
+</select>
