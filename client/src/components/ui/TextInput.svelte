f4041386a758e91b951af876850c934802f67622
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-05 17:11:02 +0800

    fmt(client): migrate to 4 space document

--- client/src/components/ui/TextInput.svelte
+++ client/src/components/ui/TextInput.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-  import { InputType } from '../types.ts';
-  
-  export let type: InputType = InputType.Text;
-  export let placeholder = '';
-  export let disabled = false;
-  export let name: string;
-  export let isRoundedBorder = true;
-  export let label: string;
+    import { InputType } from '../types.ts';
+
+    export let type: InputType = InputType.Text;
+    export let placeholder = '';
+    export let disabled = false;
+    export let name: string;
+    export let isRoundedBorder = true;
+    export let label: string;
 </script>
 
 <label>
-  {label}
-  <input {type} {placeholder} {name} {disabled} class:round-border={isRoundedBorder} />
+    {label}
+    <input {type} {placeholder} {name} {disabled} class:round-border={isRoundedBorder} />
 </label>
 
 <style>
-  @import url('../../pages/vars.css');
+    @import url('../../pages/vars.css');
 
-  input {
-    border: var(--primary-color) 2px solid;
-    padding: var(--spacing-small) var(--spacing-normal);
-  }
+    input {
+        border: var(--primary-color) 2px solid;
+        padding: var(--spacing-small) var(--spacing-normal);
+    }
 
-  .round-border {
+    .round-border {
     border-radius: var(--border-radius);
-  }
+    }
 </style>
