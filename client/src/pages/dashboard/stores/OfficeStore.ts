4aad6a15a0d8cba871b69af1a0a496c89c7597e5
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-05 17:12:08 +0800

    fix(client): initial OfficeList set to empty array

--- client/src/pages/dashboard/stores/OfficeStore.ts
+++ client/src/pages/dashboard/stores/OfficeStore.ts
@@ -7,7 +7,7 @@ import type { Office as OfficeModel } from './../../../../model/src/office.ts';
 import { Office } from '../../../api/office.ts'
 
 export const officeList: Loadable<OfficeModel[]> = asyncReadable (
-    null,
+    [],
     Office.getAll,
     { reloadable: true}
 );
