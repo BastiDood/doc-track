28072336a253560a1f7f6d4411293b67c4ff12cc
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-06 14:49:08 +0800

    feat(client): migrate EditOffice component to use select office dropdown

--- client/src/components/ui/forms/office/EditOffice.svelte
+++ client/src/components/ui/forms/office/EditOffice.svelte
@@ -2,6 +2,7 @@
     import { assert } from '../../../../assert.ts';
 
     import { Office } from '../../../../api/office.ts';
+    import { Office as OfficeModel} from '../../../../../../model/src/office.ts'
     import { InputType } from '../../../types.ts';
     import { userSession } from '../../../../pages/dashboard/stores/UserStore.ts';
     import { officeList } from '../../../../pages/dashboard/stores/OfficeStore.ts';
@@ -9,25 +10,28 @@
     import TextInput from '../../TextInput.svelte';
     import Button from '../../Button.svelte';
     import Checkmark from '../../../icons/Checkmark.svelte';
+    import OfficeSelect from '../../OfficeSelect.svelte';
+
+    let officeID: number;
+    let officeName: string;
 
     async function handleSubmit(this: HTMLFormElement) {
-        const elOfficeId = this.elements.namedItem('officeid');
-        const elOfficeName = this.elements.namedItem('officename')
-        
-        assert(elOfficeId instanceof HTMLInputElement);
+        const elOfficeName = this.elements.namedItem('officename');
+
         assert(elOfficeName instanceof HTMLInputElement);
-        assert(elOfficeId.type === 'number')
         assert(elOfficeName.type === 'text');
-        
-        if (!elOfficeId.value || !elOfficeName.value) return;
+
+        if (!officeID || !elOfficeName.value || elOfficeName.value === officeName) return;
         
         try {
             // Create a pseudo-office element
             await Office.update({
-                id: elOfficeId.value,
+                id: officeID,
                 name: elOfficeName.value
             });
             await officeList.reload?.();
+            officeID = 0;
+            officeName = '';
             this.reset();
         } catch (err) {
             // TODO: No permission handler
@@ -37,27 +41,26 @@
 </script>
 
 <p>You are currently editing an office {$userSession.email}</p>
-
 <section>
-    <!--TODO: Replace with dropdown selection implementation-->
-    {#each $officeList as office}
-        <p data-id={office.id} data-name={office.name}>{office.id}: {office.name}</p>
-    {:else}
-        No offices available
-    {/each}
+    
 </section>
 <article>
     <form on:submit|preventDefault|stopPropagation={handleSubmit}>
-        <TextInput 
+        <OfficeSelect bind:index={officeID} bind:value={officeName} options={$officeList}/>
+        <br>
+        <TextInput
             placeholder="New Office ID"
             type = {InputType.Number}
             name="officeid"
             label="Office ID:"
+            bind:value={officeID}
+            disabled={true}
         />
         <TextInput 
             placeholder="Office Name"
             name="officename"
             label="Office Name:"
+            bind:value={officeName}
         />
         <Button submit><Checkmark alt="Edit Office"/> Edit Office</Button>
     </form>
@@ -65,9 +68,4 @@
 
 <style>
     @import url('../../../../pages/vars.css');
-    section {
-        overflow-y: scroll;
-        border: var(--spacing-tiny) solid;
-        height: 50vh;
-    }
 </style>
