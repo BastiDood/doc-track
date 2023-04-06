9b901f7b7113ae4c47e77db4df29fa7cdd47b668
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-06 22:34:59 +0800

    sandbox(client): make LocalPermissions form accessible

--- client/src/pages/dashboard/views/Sandbox.svelte
+++ client/src/pages/dashboard/views/Sandbox.svelte
@@ -12,13 +12,18 @@
     import NewOffice from '../../../components/ui/forms/office/NewOffice.svelte';
     import EditOffice from '../../../components/ui/forms/office/EditOffice.svelte'
     import { userSession } from '../stores/UserStore.ts';
+    import { officeList } from '../stores/OfficeStore.ts';
+    import LocalPermissions from '../../../components/ui/forms/permissions/LocalPermissions.svelte';
+    import OfficeSelect from '../../../components/ui/OfficeSelect.svelte';
 
     let showContextMenu = false;
     let showCreateOffice = false;
     let showEditOffice = false;
+    let showLocalPermission = false;
     let currentContext: RowEvent | null = null;
     let currentlySelected = '';
     let showPermission = false;
+    let selectedOffice: number | null = null;
 
     function overflowClickHandler(e: CustomEvent) {
         if (!e.detail) return;
@@ -40,6 +45,25 @@
     Edit an Office
 </Button>
 
+<Button on:click={() => showLocalPermission = true} disabled={$officeList.length === 0}>
+    Edit Local Permission
+</Button>
+
+<Modal title="Edit Local Permissions" bind:showModal={showLocalPermission}>
+    Select an Office: <OfficeSelect bind:oid={selectedOffice} offices={$officeList}/>
+    <br>
+    {#if selectedOffice !== null}
+        
+        {#if $userSession.local_perms[selectedOffice] !== undefined }
+        <LocalPermissions user={{
+            ...$userSession,
+            permission: $userSession.local_perms[selectedOffice]
+        }} officeNo={selectedOffice} />
+        {:else}
+            Current user is not a staff of the selected office.
+        {/if}
+    {/if}
+</Modal>
 <Modal title="Edit Global Permissions" bind:showModal={showPermission}>
     <GlobalPermissions user={{
         ...$userSession,
@@ -55,10 +79,6 @@
     <EditOffice/>
 </Modal>
 
-<Select
-    bind:value={currentlySelected}
-    options={[ 'Test Office 1', 'Test Office 2', 'Test Office 3' ]}
-/>
 <p>Currently selected: {currentlySelected}</p>
 <div>
     {#each documentTest as doc}
