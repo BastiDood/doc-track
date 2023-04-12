bb9ba5f7ce8f3a872bf3760eb9b751a88fce1942
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-13 06:11:11 +0800

    feat(client): add remove category

--- client/src/components/ui/forms/category/RemoveCategory.svelte
+++ client/src/components/ui/forms/category/RemoveCategory.svelte
@@ -2,24 +2,24 @@
     import { Category as CategoryModel } from '~model/category.ts';
     import { Category } from '../../../../api/category.ts';
     import { userSession } from '../../../../pages/dashboard/stores/UserStore.ts';
-    import { categoryActiveList, categoryList } from '../../../../pages/dashboard/stores/CategoryStore.ts';
+    import { categoryList } from '../../../../pages/dashboard/stores/CategoryStore.ts';
     
     import Button from '../../Button.svelte';
     import Close from '../../../icons/Close.svelte';
     import CategorySelect from '../../CategorySelect.svelte';
+    import active from 'svelte-spa-router/active';
 
     let currId: CategoryModel['id'] | null = null;
     let currName: CategoryModel['name'] | null = null;
 
     // eslint-disable-next-line no-extra-parens
-    $: currName = $categoryActiveList.find(cat=> cat.id === currId)?.name ?? null;
+    $: currName = $categoryList.active.find(cat=> cat.id === currId)?.name ?? null;
     
     async function handleSubmit(this: HTMLFormElement) {
         if (currId === null || typeof currName !== 'string') return;
 
         try {
             await Category.remove(currId);
-            await categoryActiveList.reload?.();
             await categoryList.reload?.();
 
             // eslint-disable-next-line require-atomic-updates
@@ -38,11 +38,11 @@
 <p>You are currently renaming a category as {$userSession?.email}</p>
 
 <article>
-    {#if $categoryActiveList.length === 0}
+    {#if $categoryList.active.length === 0}
         No categories to edit.
     {:else}
         <form on:submit|preventDefault|stopPropagation={handleSubmit}>
-            <CategorySelect bind:catid={currId} categories={$categoryActiveList}/>
+            <CategorySelect bind:catid={currId} categories={$categoryList.active}/>
             <br/>
             {#if typeof currId === 'number'}
                 <p>Category ID: {currId}</p>
