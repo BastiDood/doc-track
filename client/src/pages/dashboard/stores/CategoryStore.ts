eb71a7b23eb0463cef520375ce44d53777ae4140
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-12 10:33:58 +0800

    fix(client): completely migrate store to shorthand

--- client/src/pages/dashboard/stores/CategoryStore.ts
+++ client/src/pages/dashboard/stores/CategoryStore.ts
@@ -1,15 +1,11 @@
-// HACK: IntelliSense doesn't seem to be playing nice unless we do this.
-import type { Loadable } from '@square/svelte-store/lib/async-stores/types.js';
-import { asyncReadable } from '@square/svelte-store/lib/async-stores';
-
-import type { Category as CategoryModel } from './../../../../model/src/category.ts';
+import { asyncReadable } from '@square/svelte-store';
 
 import { Category } from '../../../api/category.ts'
 
-export const activeCategoryList: Loadable<CategoryModel[]> = asyncReadable (
+export const officeList = asyncReadable(
     [],
     Category.getAllActive,
-    { reloadable: true}
+    { reloadable: true }
 );
 
 // Need some API to get all categories, even disabled ones for server operator.
