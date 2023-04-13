6ef66a8ccb185383083056bd69cfe2b3f5ae0b5d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-13 20:37:44 +0800

    fix(client): use correct default value

--- client/src/pages/dashboard/stores/OfficeStore.ts
+++ client/src/pages/dashboard/stores/OfficeStore.ts
@@ -3,7 +3,7 @@ import { asyncReadable } from '@square/svelte-store';
 import { Office } from '../../../api/office.ts';
 
 export const allOffices = asyncReadable(
-    [],
+    { },
     Office.getAll,
     { reloadable: true }
 );
