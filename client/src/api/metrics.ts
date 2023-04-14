d6341d3a498e7d0f0306f8faadb18c6a9d26febf
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-14 18:41:16 +0800

    feat(client/api): add wrapper for local summary generation

--- client/src/api/metrics.ts
+++ client/src/api/metrics.ts
@@ -1,22 +1,39 @@
 import { StatusCodes } from 'http-status-codes';
 
-import { type Metrics, MetricsSchema } from '~model/metrics.ts';
+import { type Metrics as MetricsType, MetricsSchema } from '~model/metrics.ts';
+import type { Office } from '~model/office.ts';
 
 import {
     InsufficientPermissions,
+    InvalidInput,
     InvalidSession,
     BadContentNegotiation,
     UnexpectedStatusCode,
 } from './error.ts';
 
 export namespace Metrics {
-    export async function generateUserSummary(): Promise<Metrics> {
+    export async function generateUserSummary(): Promise<MetricsType> {
         const res = await fetch('/api/metrics/user', {
             credentials: 'same-origin',
             headers: { 'Accept': 'application/json' },
         });
         switch (res.status) {
             case StatusCodes.OK: return MetricsSchema.parse(await res.json());
+            case StatusCodes.UNAUTHORIZED: throw new InvalidSession;
+            case StatusCodes.FORBIDDEN: throw new InsufficientPermissions;
+            case StatusCodes.NOT_ACCEPTABLE: throw new BadContentNegotiation;
+            default: throw new UnexpectedStatusCode;
+        }
+    }
+
+    export async function generateLocalSummary(oid: Office['id']): Promise<MetricsType> {
+        const res = await fetch(`/api/metrics/office?id=${oid}`, {
+            credentials: 'same-origin',
+            headers: { 'Accept': 'application/json' },
+        });
+        switch (res.status) {
+            case StatusCodes.OK: return MetricsSchema.parse(await res.json());
+            case StatusCodes.BAD_REQUEST: throw new InvalidInput;
             case StatusCodes.UNAUTHORIZED: throw new InvalidSession;
             case StatusCodes.FORBIDDEN: throw new InsufficientPermissions;
             case StatusCodes.NOT_ACCEPTABLE: throw new BadContentNegotiation;
