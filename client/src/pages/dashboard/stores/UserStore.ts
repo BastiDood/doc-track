b45ba2afa4ba869f2c0128722d9a3c0dd64332b1
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-15 21:21:32 +0800

    feat(client): add currentUser derived store in UserStore

--- client/src/pages/dashboard/stores/UserStore.ts
+++ client/src/pages/dashboard/stores/UserStore.ts
@@ -1,9 +1,19 @@
 import { asyncReadable } from '@square/svelte-store';
 
 import { Session } from '../../../api/session.ts';
+import { derived } from 'svelte/store';
+import { User } from '~model/user.ts';
 
 export const userSession = asyncReadable(
     null,
     Session.getUser,
     { reloadable: true }
 );
+
+export const currentUser = derived(userSession, session => ({
+    id: session?.id,
+    name: session?.name,
+    email: session?.email,
+    picture: session?.picture,
+    permission: session?.global_perms
+} as User));
