ee8d29248c7f2bab8f9f915eb17b0786bec1b626
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-15 21:39:27 +0800

    fmt(client): fix indentation and whitespace

--- client/src/components/ui/navigationbar/SideDrawer.svelte
+++ client/src/components/ui/navigationbar/SideDrawer.svelte
@@ -15,16 +15,16 @@
 
 <nav class:show={show} on:click|stopPropagation on:keypress>
     <section>
-            <a href="#/inbox" use:active><InboxIcon />Inbox</a>
-            <a href="#/outbox" use:active><OutboxIcon />Outbox</a>
-            <a href="#/drafts" use:active><EventsIcon />Drafts</a>
-            <a href="#/metrics" use:active><EventsIcon />Metrics</a>            
-            <a href="#/barcodes" use:active><BarcodesIcon />Barcodes</a>
-            <a href="#/invites" use:active><InvitesIcon />Invites</a>
-            <a href="#/staff" use:active><StaffIcon />Staff</a>
-            <a href="#/admin" use:active><AdminIcon />Admin</a>
-            <a href="#/settings" use:active><SettingsIcon />Settings</a>
-            <a href="#/sandbox" use:active><SettingsIcon />Sandbox</a>
+        <a href="#/inbox" use:active><InboxIcon />Inbox</a>
+        <a href="#/outbox" use:active><OutboxIcon />Outbox</a>
+        <a href="#/drafts" use:active><EventsIcon />Drafts</a>
+        <a href="#/metrics" use:active><EventsIcon />Metrics</a>
+        <a href="#/barcodes" use:active><BarcodesIcon />Barcodes</a>
+        <a href="#/invites" use:active><InvitesIcon />Invites</a>
+        <a href="#/staff" use:active><StaffIcon />Staff</a>
+        <a href="#/admin" use:active><AdminIcon />Admin</a>
+        <a href="#/settings" use:active><SettingsIcon />Settings</a>
+        <a href="#/sandbox" use:active><SettingsIcon />Sandbox</a>
     </section>
     <form method="POST" action="/auth/logout">
         <input type="submit" value="Logout" />
