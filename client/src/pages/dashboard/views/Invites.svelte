2211546f50fb83af4956fce53401585564efa40a
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-18 18:46:40 +0800

    feat(client): add OfficeSelect to routes/pages

--- client/src/pages/dashboard/views/Invites.svelte
+++ client/src/pages/dashboard/views/Invites.svelte
@@ -1,1 +1,15 @@
-<p>Invites!</p>
+<script lang="ts">
+    import { Office } from "~model/office";
+    import { dashboardState } from "../stores/DashboardState";
+
+    let currentOffice: Office['id'] | null = null;
+
+    $: $dashboardState.currentOffice ? currentOffice = $dashboardState.currentOffice : null;
+</script>
+
+{#if (currentOffice === null)}
+    You must select an office before accessing the Invites page.
+{:else}
+    Invites page of Office ID {currentOffice}.
+    <h1> Invites </h1>
+{/if}
