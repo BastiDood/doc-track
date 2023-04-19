26c9fdbecfd337256de3c7203b1fb569f7583a9f
Justin Ruaya <jrruaya1@up.edu.ph>
2023-04-19 11:04:18 +0800

    chore(client): move tacking view to track folder

---
+++ client/src/pages/track/Track.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    let showModal = false;
+    let title = 'Document';
+
+</script>
+
+
+<main>
+    <h2>Document {title}</h2>
+
+    <section>
+        <p class="table-title">Overview</p>
+        <table>
+            <tr>Test</tr>
+        </table>
+    </section>
+</main>
+
+<style>
+    .table-title {
+        color: var(--color-primary);
+        font-size: var(--font-size-large);
+    }
+</style>
