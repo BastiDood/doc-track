48840f28d6c67253c9c71b6851f74f6d14f71c60
Arukuen <adsarmiento3@up.edu.ph>
2023-04-26 10:40:08 +0800

    feat(client): Show the list of the earliest batch

--- client/src/components/ui/forms/batch/FetchEarliestBatch.svelte
+++ client/src/components/ui/forms/batch/FetchEarliestBatch.svelte
@@ -1,46 +1,13 @@
 <script lang="ts">
-  import { Batch } from '../../../../api/batch.ts';
-  import { Office } from '../../../../api/office.ts';
-  import { userSession } from '../../../../pages/dashboard/stores/UserStore.ts';
-  import { allOffices } from '../../../../pages/dashboard/stores/OfficeStore.ts';
-  import { Office as OfficeModel } from '../../../../../../model/src/office.ts';
-
-  import TextInput from '../../TextInput.svelte';
-  import Button from '../../Button.svelte';
-  import Checkmark from '../../../icons/Checkmark.svelte';
-  import OfficeSelect from '../../OfficeSelect.svelte';
-
-  let currId: OfficeModel['id'] | null = null;
-  let currName: OfficeModel['name'] | null = null;
-
-  $: currName = currId === null ? null : $allOffices[currId] ?? null;
-
-
-  async function handleFetch() {
-      if (currId === null || typeof currName !== 'string') return;
-      
-      try {
-          console.log(await Batch.getEarliestBatch(currId));
-      }
-      catch (err) {
-          // TODO: error message
-          alert(err);
-      }
-  }
-
+    import { userSession } from '../../../../pages/dashboard/stores/UserStore.ts';
+    import { earliestBatch } from '../../../../pages/dashboard/stores/BatchStore.ts'
 </script>
 
-<p>You are currently viewing that batch of barcodes as {$userSession?.email}</p>
-<article>
-  {#if Object.getOwnPropertyNames($allOffices).length === 0}
-      No office where a new batch of barcodes will be viewed
-  {:else}
-      <form on:submit|preventDefault|stopPropagation={handleFetch}>   
-          <OfficeSelect bind:oid={currId} offices={$allOffices} />
-          <br />
-          {#if currName !== null}
-              <Button submit><Checkmark alt="Download Barcodes"/>Download</Button> 
-          {/if}
-      </form>
-  {/if}
-</article>
+<p>You successfully fetched the earliest batch as {$userSession?.email}</p>
+{#if $earliestBatch?.codes !== undefined}
+    <ul>
+        {#each $earliestBatch?.codes as code}
+            <li>{code}</li>   
+        {/each}
+    </ul>
+{/if}
