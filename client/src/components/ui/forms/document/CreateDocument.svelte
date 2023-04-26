fb6127b95cf7d2107eb1a5896af238597c812cf2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-26 10:46:52 +0800

    feat(client): implement document creation form

---
+++ client/src/components/ui/forms/document/CreateDocument.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { assert } from '../../../../assert.ts';
+    import { Document as Api } from '../../../../api/document.ts';
+
+    import { categoryList } from '../../../../pages/dashboard/stores/CategoryStore.ts';
+    import { dashboardState } from '../../../../pages/dashboard/stores/DashboardState.ts';
+
+    import type { Category } from '../../../../.../../../../model/src/category.ts';
+    import type { Document } from '../../../../.../../../../model/src/document.ts';
+    import type { Snapshot } from '../../../../.../../../../model/src/snapshot.ts';
+
+    import Button from '../../Button.svelte';
+    import CategorySelect from '../../CategorySelect.svelte';
+    import TextInput from '../../TextInput.svelte';
+
+    let id: Document['id'] | null = null;
+    let category: Category['id'] | null = null;
+    let title: Document['title'] = '';
+    let remark: Snapshot['remark'] = '';
+
+    async function handleSubmit(this: HTMLFormElement) {
+        const oid = $dashboardState.currentOffice;
+        if (oid === null || id === null || category === null) return;
+
+        const result = await Api.create(oid, { id, title, category }, remark);
+        assert(result instanceof Date);
+    }
+</script>
+
+<form on:submit|preventDefault|stopPropagation={handleSubmit}>
+    <!-- TODO: Barcode Select -->
+    <TextInput placeholder="Document Title..." name="title" label="Title"></TextInput>
+    <CategorySelect bind:catId={category} categories={$categoryList.active} />
+    <TextInput placeholder="Remarks..." name="remark" label="Remark" required={false}></TextInput>
+    <Button submit>Create Document</Button>
+</form>
