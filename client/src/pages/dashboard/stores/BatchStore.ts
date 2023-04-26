ed8e792e24113a02da7988ecc2028e93989fe86c
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-26 11:05:16 +0800

    refactor(client): prefer ternary operators

--- client/src/pages/dashboard/stores/BatchStore.ts
+++ client/src/pages/dashboard/stores/BatchStore.ts
@@ -2,12 +2,13 @@ import { asyncDerived } from '@square/svelte-store';
 import { dashboardState } from './DashboardState';
 import { Batch } from '../../../api/batch';
 
-export const earliestBatch = asyncDerived(dashboardState, ($dashboardState) => {
-  if ($dashboardState.currentOffice === null){
-    return Promise.resolve({})
-  }
-  return Batch.getEarliestBatch($dashboardState.currentOffice);
-  }, 
-  { reloadable: true }
+export const earliestBatch = asyncDerived(
+    dashboardState,
+    $dashboardState => {
+    const { currentOffice } = $dashboardState;
+    return currentOffice === null
+        ? Promise.resolve({})
+        : Batch.getEarliestBatch(currentOffice);
+    },
+    { reloadable: true }
 );
-
