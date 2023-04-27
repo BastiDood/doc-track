2f69e4f351615c4c4c8aea718b34c55db1ba0086
Arukuen <adsarmiento3@up.edu.ph>
2023-04-27 20:37:48 +0800

    feat(client): add modal for UnsubscribePushNotification

---
+++ client/src/components/ui/forms/pushnotification/UnsubscribePushNotification.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    
+    import Button from '../../Button.svelte';
+    import Checkmark from '../../../icons/Checkmark.svelte';
+
+    import { IconColor } from '../../../types.ts';
+
+    const dispatch = createEventDispatcher();
+
+    function unsubscribe() {
+        dispatch('unsubscribe', {
+            isSubscribed: false 
+        });
+    }
+</script>
+
+<p>Do you want to Unsubscribe to Push Notification?</p>
+<Button on:click={unsubscribe}>
+    <Checkmark color={IconColor.White} alt="Accept Push Notification" on:click/>Unsubscribe
+</Button>
+
