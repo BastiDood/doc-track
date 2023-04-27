210c6123d93562ad18e7fcfd29d5840ca7b570e6
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-28 04:41:26 +0800

    fix(client): prefer Square Svelte stores

--- client/src/pages/dashboard/stores/CurrentPage.ts
+++ client/src/pages/dashboard/stores/CurrentPage.ts
@@ -1,4 +1,4 @@
-import { derived } from 'svelte/store';
+import { derived } from '@square/svelte-store';
 import { location } from 'svelte-spa-router';
 
 export const currentPage = derived(location, $location => $location.charAt(1).toUpperCase() + $location.slice(2));
