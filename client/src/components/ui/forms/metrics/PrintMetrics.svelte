10074968fadaa90067de48fb2fb44bd3593c9633
Arukuen <adsarmiento3@up.edu.ph>
2023-04-27 22:22:35 +0800

    feat(client): add modal for printing the metrics

---
+++ client/src/components/ui/forms/metrics/PrintMetrics.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { userSession } from '../../../../pages/dashboard/stores/UserStore.ts';
+    import { Metrics as MetricsModel } from '~model/metrics.ts';
+
+    export let register: MetricsModel['Register'];
+    export let send: MetricsModel['Send'];
+    export let receive: MetricsModel['Receive'];
+    export let terminal: MetricsModel['Terminate'];
+</script>
+
+<p>You successfully fetched the metrics as {$userSession?.email}</p>
+<ul>
+    <li>Registerd: {register}</li>
+    <li>Send: {send}</li>
+    <li>Receive: {receive}</li>
+    <li>Terminal: {terminal}</li>
+</ul>
+
