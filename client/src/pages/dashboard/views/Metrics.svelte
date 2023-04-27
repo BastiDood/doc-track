71f8c4f5224985169c084e3bf986aea91dbd4799
Arukuen <adsarmiento3@up.edu.ph>
2023-04-27 22:23:23 +0800

    feat(client): apply the metrics api to the subpage

--- client/src/pages/dashboard/views/Metrics.svelte
+++ client/src/pages/dashboard/views/Metrics.svelte
@@ -2,73 +2,75 @@
     import { Office } from '~model/office';
     import { dashboardState } from '../stores/DashboardState.ts';
     import { Metrics } from '../../../api/metrics.ts';
+    import { Metrics as MetricsModel } from '~model/metrics.ts';
 
     import Button from '../../../components/ui/Button.svelte';
-    import { ButtonType } from '../../../components/types.ts';
-
     import Download from '../../../components/icons/Download.svelte';
-    import Close from '../../../components/icons/Close.svelte';
+    import PrintMetrics from '../../../components/ui/forms/metrics/PrintMetrics.svelte';
+    import Modal from '../../../components/ui/Modal.svelte';
 
     let currentOffice: Office['id'] | null = null;
 
     // eslint-disable-next-line prefer-destructuring
     $: if ($dashboardState.currentOffice !== null) currentOffice = $dashboardState.currentOffice;
 
-    let outbox = 0;
-    let created = 0;
-    let received = 0;
-    let sent = 0;
-    let terminal = 0;
+    let register: MetricsModel['Register'] = 0;
+    let send: MetricsModel['Send'] = 0;
+    let receive: MetricsModel['Receive'] = 0;
+    let terminal: MetricsModel['Terminate'] = 0;
 
-    async function handlePrint() {
+    let showPrintMetrics = false;
+    
+    async function loadMetrics() {
         if (currentOffice === null) return;
-        try {
-            console.log(await Metrics.generateLocalSummary(currentOffice));
-        } catch (err) {
-            // TODO: error message
-            alert(err);
-        }
+        let localMetric = await Metrics.generateLocalSummary(currentOffice);
+        register = localMetric.Register === undefined ? 0 : localMetric.Register;
+        send = localMetric.Send  === undefined ? 0 : localMetric.Send;
+        receive = localMetric.Receive  === undefined ? 0 : localMetric.Receive;
+        terminal = localMetric.Terminate  === undefined ? 0 : localMetric.Terminate;
     }
 
-    function handleReset () {
-
+    $: {
+        currentOffice;
+        loadMetrics();
     }
 </script>
 
-<main>
-    <div class='header'>
-        <h3>Report</h3>
-        <Button on:click={handlePrint}>
-            <Download alt='download' />Print Report
-        </Button>
-        <Button type={ButtonType.Danger}>
-            <Close alt='close' />Reset
-        </Button>
-    </div>
+{#if currentOffice === null}
+    You must select an office before accessing the Barcodes page.
+{:else}
+    <main>
+        <div class='header'>
+            <h3>Report</h3>
+            <Button on:click={() => showPrintMetrics = true}>
+                <Download alt='download' />Print Report
+            </Button>
+        </div>
 
-    <table>
-        <tr>
-            <td>Outbox</td>
-            <td>{{outbox}}</td>
-        </tr>
-        <tr>
-            <td>Documents Created</td>
-            <td>{{created}}</td>
-        </tr>
-        <tr>
-            <td>Received</td>
-            <td>{{received}}</td>
-        </tr>
-        <tr>
-            <td>Sent</td>
-            <td>{{sent}}</td>
-        </tr>
-        <tr>
-            <td>Tagged as Terminal</td>
-            <td>{{terminal}}</td>
-        </tr>
-    </table>
-</main>
+        <table>
+            <tr>
+                <td>Registered</td>
+                <td>{register}</td>
+            </tr>
+            <tr>
+                <td>Sent</td>
+                <td>{send}</td>
+            </tr>
+            <tr>
+                <td>Received</td>
+                <td>{receive}</td>
+            </tr>
+            <tr>
+                <td>Tagged as Terminal</td>
+                <td>{terminal}</td>
+            </tr>
+        </table>
+
+        <Modal title="Print Metrics" bind:showModal={showPrintMetrics}>
+            <PrintMetrics register={register} send={send} receive={receive} terminal={terminal} />
+        </Modal>
+    </main>
+{/if}
 
 
 <style>
