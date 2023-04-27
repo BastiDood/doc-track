1fe0b5319ee1aba189160df0cb0494a3f6472b19
Arukuen <adsarmiento3@up.edu.ph>
2023-04-27 20:37:15 +0800

    feat(client): add modal for SubscribePushNotification

---
+++ client/src/components/ui/forms/pushnotification/SubscribePushNotification.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    import Button from '../../Button.svelte';
+    import Checkmark from '../../../icons/Checkmark.svelte';
+
+    import { IconColor } from '../../../types.ts';
+
+	const dispatch = createEventDispatcher();
+
+    function subscribe() {
+        dispatch('subscribe', {
+			isSubscribed: true 
+		});
+    }
+</script>
+
+<p>Do you want to Subscribe to Push Notification?</p>
+<Button on:click={subscribe}>
+    <Checkmark color={IconColor.White} alt="Accept Push Notification"/>Subcribe
+</Button>
