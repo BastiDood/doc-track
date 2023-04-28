75c7735e7692c102b56054b970044137b3b919b3
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-28 19:33:46 +0800

    fix(client): remove required stance on OfficeSelect

--- client/src/components/ui/OfficeSelect.svelte
+++ client/src/components/ui/OfficeSelect.svelte
@@ -4,7 +4,7 @@
     export let oid: number | null = null;
 </script>
 
-<select required bind:value={oid}>
+<select bind:value={oid}>
     {#each Object.entries(offices) as [id, office] (id)}
         <option value={id}>{office}</option>
     {/each}
