98c6a19b690354600645b91c7f72ac5169961de1
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-28 10:18:52 +0800

    fix(server): change init sql CHECK condition for snapshot target

--- server/db/init.sql
+++ server/db/init.sql
@@ -96,7 +96,7 @@ CREATE TABLE snapshot(
     target SMALLINT
         REFERENCES office (id)
         DEFAULT NULL
-        CHECK((status IN ('Send', 'Receive') AND target IS NOT NULL) OR (status NOT IN ('Send', 'Receive') AND target IS NULL)),
+        CHECK((status IN ('Terminate') AND target is NULL) OR (status NOT IN ('Send', 'Receive', 'Register') AND target is NOT NULL)),
     status DocStatus NOT NULL DEFAULT 'Register',
     remark VARCHAR(32) NOT NULL,
     PRIMARY KEY (creation, doc)
