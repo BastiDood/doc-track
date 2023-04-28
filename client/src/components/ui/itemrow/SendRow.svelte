44f9b310d68be9c971814f952f0289c4fca65edb
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-28 23:42:12 +0800

    refactor(client): enforce that the target must be non-null

--- client/src/components/ui/itemrow/SendRow.svelte
+++ client/src/components/ui/itemrow/SendRow.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
+    import type { Document } from '~model/document.ts';
+    import type { Category } from '~model/category.ts';
+    import type { Office } from '~model/office.ts';
+
     import DocumentExport from '../../icons/DocumentExport.svelte';
     import RowTemplate from '../RowTemplate.svelte';
 
     import { IconSize } from '../../types.ts';
-    import { Document } from '../../../../../model/src/document.ts';
-    import { Category } from '~model/category.ts';
     
     export let iconSize: IconSize;
     export let doc: Document['id'];
     export let category: Category['name'];
     export let title: Document['title'];
-    export let target: number | null;
-
-    $: target ??= 0;
+    export let target: Office['id'];
 </script>
 
 <RowTemplate {iconSize} title={`${title} ID: ${doc} Category: ${category} Sent to Office: ${target}`}>
