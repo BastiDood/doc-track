1edeb0225bf2a37e2e113d623b9ecdffabe1880d
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-04-28 19:37:53 +0800

    feat(client): create new RegisterRow component

---
+++ client/src/components/ui/itemrow/RegisterRow.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    import DocumentBlank from '../../icons/DocumentBlank.svelte';
+    import RowTemplate from '../RowTemplate.svelte';
+
+    import { IconSize, RowType, ContextPayload, Events } from '../../types.ts';
+    import { Document } from '../../../../../model/src/document.ts';
+    import { Category } from '~model/category.ts';
+    
+    export let iconSize: IconSize;
+    export let doc: Document['id'];
+    export let category: Category['name'];
+    export let title: Document['title'];
+
+    const dispatch = createEventDispatcher();
+    const rowEvent: ContextPayload = {
+        ty: RowType.Inbox,
+        id: doc,
+    };
+</script>
+
+<RowTemplate
+    title={`${title} ID: ${doc} Category: ${category}`}
+    {iconSize} 
+    on:overflowClick={() => dispatch(Events.OverflowClick, rowEvent)}
+>
+    <DocumentBlank size={iconSize} slot="icon" alt ="An sent document" />
+</RowTemplate>
