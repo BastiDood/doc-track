8143fdc9170c75f053d64d88b705b9880d6b7b67
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-30 14:57:55 +0800

    feat(client): implement `StatusSelect` component

---
+++ client/src/components/ui/StatusSelect.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Status } from '~model/snapshot.ts';
+    export let value: number | undefined;
+</script>
+
+<select required bind:value>
+    <option value={Status.Register}>Register</option>
+    <option value={Status.Send}>Send</option>
+    <option value={Status.Receive}>Receive</option>
+    <option value={Status.Terminate}>Terminate</option>
+</select>
+
+<style>
+    select {
+        padding: var(--spacing-small) var(--spacing-normal);
+        border: var(--spacing-tiny) solid var(--shadow-color);
+        border-radius: var(--border-radius);
+        background: var(--dashboard-bg);
+        box-shadow: 0 var(--spacing-tiny) var(--spacing-small) -0.0625em var(--shadow-color);
+        cursor: pointer;
+        font-size: var(--normal);
+        transition: all var(--animation-length) ease;
+    }
+
+    select:focus {
+        outline: none;
+        border-color: var(--primary-color);
+        box-shadow: 0 0 0 var(--spacing-tiny) rgba(var(--primary-color), 0.2);
+    }
+</style>
