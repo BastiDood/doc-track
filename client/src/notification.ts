3d497725e55d858b0df060a1f029d86c98d7bbb0
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-30 18:35:46 +0800

    feat(client): implement notification request helper

---
+++ client/src/notification.ts
@@ -0,0 +1,13 @@
+/** Attempts to send a notification. Repeatedly prompts the user until they explicitly accept or reject. */
+export async function sendNotification(title: string, options?: NotificationOptions) {
+    let perm = Notification.permission;
+    do {
+        switch (perm) {
+            case 'granted': return new Notification(title, options);
+            case 'denied': return null;
+            case 'default':
+            default: break;
+        }
+        perm = await Notification.requestPermission();
+    } while (true);
+}
