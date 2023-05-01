71a40ccb840ef6f6ae01345f7415745135830f08
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-05-01 09:58:41 +0800

    feat(client): implemented css grids on RowTemplate

--- client/src/components/ui/RowTemplate.svelte
+++ client/src/components/ui/RowTemplate.svelte
@@ -6,38 +6,55 @@
     import OverflowMenuVertical from '../icons/OverflowMenuVertical.svelte';
 
     export let iconSize: IconSize = IconSize.Large;
-    export let title: string;
+    export let showOverflowIcon: boolean = true;
 
     const dispatch = createEventDispatcher();
 </script>
 
 <article>
     <div class="icon"><slot name="icon" /></div>
-    <p>{title}</p>
-    <div class="icon" on:keydown on:click|stopPropagation={() => dispatch(Events.OverflowClick)}>
-        <OverflowMenuVertical size={iconSize} alt="Show overflow menu" />
-    </div>
+    <div class="header"><slot/></div>
+    <div class="subtext"><slot name="secondary"/></div>
+    {#if showOverflowIcon}
+        <div class="overflow" on:keydown on:click|stopPropagation={() => dispatch(Events.OverflowClick)}>
+            <OverflowMenuVertical size={iconSize} alt="Show overflow menu" />
+        </div>
+    {/if}
 </article>
 
 <style>
     @import url('../../pages/vars.css');
 
     article {
-        display: flex;
+        display: grid;
         border-style: solid;
         border-width: var(--spacing-tiny);
         border-radius: var(--border-radius);
         margin: var(--spacing-small);
         padding: var(--spacing-small);
-
+        grid-row-gap: var(--spacing-small);
+        justify-content: space-between;
+        grid-template-areas: 
+            "a b b b b b c"
+            "a d d d d d c";
     }
 
     .icon {
-        flex-grow: 0;
+        grid-area: a;
+        align-self: start;
     }
 
-    p {
-        flex-grow: 1;
-        margin: 0;
+    .overflow {
+        grid-area: c;
+        align-self: start;
+    }
+    
+    .header {
+        grid-area: b;
+    }
+
+    .subtext {
+        grid-area: d;
+        font-size: var(--small);
     }
 </style>
