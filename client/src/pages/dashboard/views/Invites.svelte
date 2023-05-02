e3097b55c8170b23f849036cd2e5c0b073fac46b
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-03 02:57:30 +0800

    feat(client): invite rows now work, yay

--- client/src/pages/dashboard/views/Invites.svelte
+++ client/src/pages/dashboard/views/Invites.svelte
@@ -1,9 +1,13 @@
-<script>
+<script lang="ts">
     import { dashboardState } from '../stores/DashboardState';
     import InviteForm from '../../../components/ui/forms/office/AddInvite.svelte';
     import Modal from '../../../components/ui/Modal.svelte';
     import Button from '../../../components/ui/Button.svelte';
+    import InviteRow from '../../../components/ui/itemrow/InviteRow.svelte';
+    import { inviteList } from '../../../pages/dashboard/stores/InviteStore.ts';
 
+    import type { Invitation } from '../../../../model/src/invitation.ts';
+    import { IconSize } from '../../../components/types.ts';
 
     let showInviteForm = false;
     $: ({ currentOffice } = $dashboardState);
@@ -22,4 +26,17 @@
             <InviteForm />
         {/if}
     </Modal>
+    {#if typeof $inviteList === 'undefined' || $inviteList.length === 0}
+            No invite backlogs, yay!
+    {:else}
+        {#each $inviteList as { email, permission, creation } (email)}
+            <InviteRow
+                email={email}
+                office={$dashboardState.currentOffice ?? 0}
+                permission={permission}
+                iconSize={IconSize.Large}
+                creation={creation}
+            />
+        {/each}
+    {/if}
 {/if}
