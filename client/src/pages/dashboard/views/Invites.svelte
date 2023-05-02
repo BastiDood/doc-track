33428f366baa0c4b541d27d9db13c290f5929600
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-03 01:21:41 +0800

    chore(client): move AddInvite to Invites tab

--- client/src/pages/dashboard/views/Invites.svelte
+++ client/src/pages/dashboard/views/Invites.svelte
@@ -1,12 +1,25 @@
 <script>
     import { dashboardState } from '../stores/DashboardState';
+    import InviteForm from '../../../components/ui/forms/office/AddInvite.svelte';
+    import Modal from '../../../components/ui/Modal.svelte';
+    import Button from '../../../components/ui/Button.svelte';
 
+
+    let showInviteForm = false;
     $: ({ currentOffice } = $dashboardState);
 </script>
 
 {#if currentOffice === null}
     You must select an office before accessing the Invites page.
 {:else}
-    Invites page of Office ID {currentOffice}.
-    <h1>Invites</h1>
+    <Button on:click={() => (showInviteForm = true)}>
+        Invite User
+    </Button>
+    <Modal title="Invite User" bind:showModal={showInviteForm}>
+        {#if $dashboardState.currentOffice === null}
+            <span>No selected office.</span>
+        {:else}
+            <InviteForm />
+        {/if}
+    </Modal>
 {/if}
