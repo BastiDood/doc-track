d0bd7d5b771688bd594f554761faf4769efcffaa
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-03 10:56:01 +0800

    fmt(client): remove extra newline

--- client/src/notification.ts
+++ client/src/notification.ts
@@ -1,7 +1,6 @@
 /** Attempts to send a notification. Repeatedly prompts the user until they explicitly accept or reject. */
 export async function sendNotification(title: string, options?: NotificationOptions) {
     let perm = Notification.permission;
-
     for (;;) {
         switch (perm) {
             case 'granted': return new Notification(title, options);
