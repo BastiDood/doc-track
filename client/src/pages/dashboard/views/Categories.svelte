a9bd60b3e748e9954dd600a60b68374a5656a0a8
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-05 13:59:14 +0800

    feat(client): render the active and retired categories

--- client/src/pages/dashboard/views/Categories.svelte
+++ client/src/pages/dashboard/views/Categories.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { categoryList } from '../stores/CategoryStore.ts';
+    import { IconSize } from '../../../components/types.ts';
 
+    import Add from '../../../components/icons/Add.svelte';
+    import Close from '../../../components/icons/Close.svelte';
     import DocumentBlank from '../../../components/icons/DocumentBlank.svelte';
     import RowTemplate from '../../../components/ui/RowTemplate.svelte';
 
@@ -10,9 +13,11 @@
 <article>
     <section>
         <h1>Active Categories</h1>
-        {#each active as { id, name: title } (id)}
-            <RowTemplate {title}>
-                <DocumentBlank slot="icon" alt="Icon for {title}" />
+        {#each active as { id, name } (id)}
+            <RowTemplate>
+                {name}
+                <DocumentBlank slot="icon" alt="Document Icon" />
+                <Close slot="overflow" size={IconSize.Large} alt="Remove Category" />
             </RowTemplate>
         {:else}
             No active categories.
@@ -20,9 +25,11 @@
     </section>
     <section>
         <h1>Retired Categories</h1>
-        {#each retire as { id, name: title } (id)}
-            <RowTemplate {title}>
-                <DocumentBlank slot="icon" alt="Icon for {title}" />
+        {#each retire as { id, name} (id)}
+            <RowTemplate>
+                {name}
+                <DocumentBlank slot="icon" alt="Document Icon" />
+                <Add slot="overflow" size={IconSize.Large} alt="Reinstate Category" />
             </RowTemplate>
         {:else}
             No retired categories.
