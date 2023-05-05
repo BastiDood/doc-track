519c4906ed6aac7198bfd30e2d9f173521e463b6
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-05 14:20:12 +0800

    feat(client): add push notifications for category management
    In the future, we should prefer less intrusive toast messages instead.

--- client/src/pages/dashboard/views/Categories.svelte
+++ client/src/pages/dashboard/views/Categories.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+    import type { Category } from '~model/category.ts';
+
+    import { Category as Api } from '../../../api/category.ts';
     import { categoryList } from '../stores/CategoryStore.ts';
     import { IconSize } from '../../../components/types.ts';
+    import { sendNotification } from '../../../notification.ts';
 
     import Add from '../../../components/icons/Add.svelte';
     import Close from '../../../components/icons/Close.svelte';
@@ -8,13 +12,38 @@
     import RowTemplate from '../../../components/ui/RowTemplate.svelte';
 
     $: ({ active, retire } = $categoryList);
+
+    async function activateCategory(cid: Category['id']) {
+        // TODO: Prefer less intrusive toast messages rather than notifications
+        const name = await Api.activate(cid);
+        if (name === null) await sendNotification('Category Activation', { body: `Failed to activate non-existent category "${name}".` });
+        else await sendNotification('Category Activation', { body: `You just reactivated category "${name}".` });
+        await categoryList.reload?.();
+    }
+
+    async function removeCategory(cid: Category['id']) {
+        // TODO: Prefer less intrusive toast messages rather than notifications
+        const result = await Api.remove(cid);
+        switch (result) {
+            case true:
+                await sendNotification('Category Deprecation', { body: 'Successfully removed the category from the system.' });
+                break;
+            case false:
+                await sendNotification('Category Deprecation', { body: 'Successfully deprecated the category.' });
+                break;
+            case null:
+                await sendNotification('Category Deprecation', { body: 'Failed to remove non-existent category.' });
+                break;
+        }
+        await categoryList.reload?.();
+    }
 </script>
 
 <article>
     <section>
         <h1>Active Categories</h1>
         {#each active as { id, name } (id)}
-            <RowTemplate>
+            <RowTemplate on:overflowClick={() => removeCategory(id)}>
                 {name}
                 <DocumentBlank slot="icon" alt="Document Icon" />
                 <Close slot="overflow" size={IconSize.Large} alt="Remove Category" />
@@ -26,7 +55,7 @@
     <section>
         <h1>Retired Categories</h1>
         {#each retire as { id, name} (id)}
-            <RowTemplate>
+            <RowTemplate on:overflowClick={() => activateCategory(id)}>
                 {name}
                 <DocumentBlank slot="icon" alt="Document Icon" />
                 <Add slot="overflow" size={IconSize.Large} alt="Reinstate Category" />
