a4f5f1d12dca412708514312559a0ff3256848d3
Arukuen <adsarmiento3@up.edu.ph>
2023-05-06 17:27:32 +0800

    fix(client): use list of users instead of staffs

--- client/src/pages/dashboard/views/Users.svelte
+++ client/src/pages/dashboard/views/Users.svelte
@@ -1,73 +1,70 @@
 <script lang="ts">
-  import { dashboardState } from '../stores/DashboardState';
-  import { staffList } from '../stores/StaffStore';
-  import { allOffices } from '../stores/OfficeStore';
+    import { dashboardState } from '../stores/DashboardState';
+    import { userList } from '../stores/UserStore';
+    import { allOffices } from '../stores/OfficeStore';
 
-  import { IconSize, PersonPayload, RowType, Events } from '../../../components/types';
-  import PersonRowGlobal from '../../../components/ui/itemrow/PersonRowGlobal.svelte';
-  import GlobalPermissions from '../../../components/ui/forms/permissions/GlobalPermissions.svelte';
-  import Modal from '../../../components/ui/Modal.svelte';
-  import PersonContextGlobal from '../../../components/ui/contextdrawer/PersonContextGlobal.svelte';
+    import { IconSize, PersonPayload, RowType, Events } from '../../../components/types';
+    import PersonRowGlobal from '../../../components/ui/itemrow/PersonRowGlobal.svelte';
+    import GlobalPermissions from '../../../components/ui/forms/permissions/GlobalPermissions.svelte';
+    import Modal from '../../../components/ui/Modal.svelte';
+    import PersonContextGlobal from '../../../components/ui/contextdrawer/PersonContextGlobal.svelte';
 
-  $: ({ currentOffice } = $dashboardState);
-  $: officeName = currentOffice === null ? 'No office name.' : $allOffices[currentOffice];
+    $: ({ currentOffice } = $dashboardState);
+    $: officeName = currentOffice === null ? 'No office name.' : $allOffices[currentOffice];
 
-  let showContextMenu = false;
-  let showGlobalPermission = false;
-  let currentContext = null as PersonPayload | null;
+    let showContextMenu = false;
+    let showGlobalPermission = false;
+    let currentContext = null as PersonPayload | null;
 
-  function overflowClickHandler(e: CustomEvent<PersonPayload>) {
+    function overflowClickHandler(e: CustomEvent<PersonPayload>) {
         currentContext = e.detail;
         showContextMenu = true;
-  }
+    }
 
-  function contextMenuHandler(e: CustomEvent<PersonPayload>) {
+    function contextMenuHandler(e: CustomEvent<PersonPayload>) {
         switch (e.type) {
             case Events.EditGlobalPermission:
                 showGlobalPermission = true;
                 break;
             default: break;
         }
-  }
+    }
 </script>
 
-{#if currentOffice === null}
-    You must select an office before accessing the Users page.
+{#await userList.load()}
+    <p>Loading users page...</p>
+{:then}
+    <h1>Users</h1>
+    {#if $userList.length === 0}
+        No users exist
     {:else}
-    {#await staffList.load()}
-        <p>Loading users page...</p>
-    {:then}
-        <h1>Users of {officeName}</h1>
-        {#if $staffList === null}
-            No users belonging in {officeName}
-        {:else}
-            {#each $staffList as staff}
-                <!-- Filtering removed staff -->
-                {#if staff.permission !== 0}
-                    <PersonRowGlobal
-                        {...staff}
-                        iconSize={IconSize.Large} 
-                        on:overflowClick={overflowClickHandler} 
-                    />
-                {/if}
-            {/each}
-        {/if}
-    {/await}
-
-    {#if currentContext?.ty === RowType.Person}
-        <PersonContextGlobal
-            bind:show={showContextMenu}
-            payload={currentContext} 
-            on:editGlobalPermission={contextMenuHandler}
-            on:removeStaff={contextMenuHandler}
-        />
+        {#each $userList as user}
+            <!-- Filtering removed users -->
+            {#if user.permission !== 0}
+                <PersonRowGlobal
+                    {...user}
+                    iconSize={IconSize.Large} 
+                    on:overflowClick={overflowClickHandler} 
+                />
+            {/if}
+        {/each}
     {/if}
+{/await}
 
-    <Modal title="Edit Global Permissions" bind:showModal={showGlobalPermission}>
-        {#if currentContext === null}
-            Current user is not a staff of the selected office.
-        {:else}
-            <GlobalPermissions payload={currentContext} />
-        {/if}
-    </Modal>
+{#if currentContext?.ty === RowType.Person}
+    <PersonContextGlobal
+        bind:show={showContextMenu}
+        payload={currentContext} 
+        on:editGlobalPermission={contextMenuHandler}
+        on:removeStaff={contextMenuHandler}
+    />
 {/if}
+
+<Modal title="Edit Global Permissions" bind:showModal={showGlobalPermission}>
+    {#if currentContext === null}
+        Current user is non existent
+    {:else}
+        <GlobalPermissions payload={currentContext} />
+    {/if}
+</Modal>
+
