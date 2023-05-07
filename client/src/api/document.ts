37fb60ea7dcc391a409a1ff6318aee0673a7b93e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-07 13:49:47 +0800

    feat(client/api): implement API wrapper for dossier getter

--- client/src/api/document.ts
+++ client/src/api/document.ts
@@ -4,14 +4,16 @@ import type { Document as DocumentType } from '~model/document.ts';
 import type { Office } from '~model/office.ts';
 
 import {
-    type BarcodeAssignmentError,
     type AllInbox,
     type AllOutbox,
+    type BarcodeAssignmentError,
+    type InboxEntry,
     type PaperTrail,
-    BarcodeAssignmentErrorSchema,
-    PaperTrailSchema,
     AllInboxSchema,
     AllOutboxSchema,
+    BarcodeAssignmentErrorSchema,
+    InboxEntrySchema,
+    PaperTrailSchema,
 } from '../../../model/src/api.ts';
 import { type Snapshot, SnapshotSchema } from '../../../model/src/snapshot.ts';
 
@@ -41,6 +43,21 @@ export namespace Document {
         switch (res.status) {
             case StatusCodes.CREATED: return SnapshotSchema.shape.creation.parse(await res.json());
             case StatusCodes.CONFLICT: return BarcodeAssignmentErrorSchema.parse(await res.json());
+            case StatusCodes.BAD_REQUEST: throw new InvalidInput;
+            case StatusCodes.UNAUTHORIZED: throw new InvalidSession;
+            case StatusCodes.FORBIDDEN: throw new InsufficientPermissions;
+            case StatusCodes.NOT_ACCEPTABLE: throw new BadContentNegotiation;
+            default: throw new UnexpectedStatusCode;
+        }
+    }
+
+    export async function getDossier(oid: Office['id']): Promise<InboxEntry[]> {
+        const res = await fetch(`/api/dossier?office=${oid}`, {
+            credentials: 'same-origin',
+            headers: { 'Accept': 'application/json' },
+        });
+        switch (res.status) {
+            case StatusCodes.OK: return InboxEntrySchema.array().parse(await res.json());
             case StatusCodes.BAD_REQUEST: throw new InvalidInput;
             case StatusCodes.UNAUTHORIZED: throw new InvalidSession;
             case StatusCodes.FORBIDDEN: throw new InsufficientPermissions;
