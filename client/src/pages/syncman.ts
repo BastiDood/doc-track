042730132edeaae2551ccd9f8517fe9b4b93d4e0
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-05-09 17:25:16 +0800

    feat(client): define SyncManager interface and global

---
+++ client/src/pages/syncman.ts
@@ -0,0 +1,34 @@
+// HACK: SyncManager is not properly defined ANYWHERE. See https://stackoverflow.com/questions/69786676/how-to-use-service-worker-background-sync-with-typescript
+export interface SyncManager {
+    getTags(): Promise<string[]>;
+    register(tag: string): Promise<void>;
+}
+
+declare global {
+    interface ServiceWorkerRegistration {
+        readonly sync: SyncManager;
+    }
+
+    interface SyncEvent extends ExtendableEvent {
+        readonly lastChance: boolean;
+        readonly tag: string;
+    }
+
+    interface ServiceWorkerGlobalScopeEventMap {
+        sync: SyncEvent;
+    }
+
+    // eslint-disable-next-line init-declarations
+    let SyncEvent: {
+      prototype: SyncEvent;
+        new(type: string, eventInitDict?: ExtendableEvent): SyncEvent;
+  };
+}
+
+export interface DeferredFetch {
+    credentials: RequestCredentials
+    url: string;
+    method: string;
+    body: BodyInit;
+    headers: HeadersInit;
+}
