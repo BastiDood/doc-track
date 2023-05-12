0efe0695365b9d6f68ac82f031629fb7f47a2319
Arukuen <adsarmiento3@up.edu.ph>
2023-05-12 08:58:31 +0800

    fix(client): add display when no earliest batch

--- client/src/components/ui/forms/batch/FetchEarliest.svelte
+++ client/src/components/ui/forms/batch/FetchEarliest.svelte
@@ -3,8 +3,10 @@
     import { earliestBatch } from '../../../../pages/dashboard/stores/BatchStore.ts';
 </script>
 
-<p>You successfully fetched the earliest batch as {$userSession?.email}</p>
-{#if typeof $earliestBatch?.codes !== 'undefined'}
+{#if typeof $earliestBatch?.codes === 'undefined'}
+    <p>No batch available.</p>
+{:else}
+    <p>You successfully fetched the earliest batch as {$userSession?.email}</p>
     <ul>
         {#each $earliestBatch.codes as code}
             <li>{code}</li>   
