a74c4a5a80e4f2718734373acd6f5ff62359dd3b
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-18 07:18:27 +0800

    feat(client): Print QR Code button

---
+++ client/src/components/ui/qr/PrintQr.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import DownloadButton from '../../icons/DocumentDownload.svelte'; 
+    import Button from '../Button.svelte';
+    import Modal from '../Modal.svelte';
+    import QrGenerator from './QrGenerator.svelte';
+    import { ButtonType } from '../../types.ts';
+
+    export let trackingNumber: string;
+    const trackingUrl = `localhost:3000/track?id=${trackingNumber}`;
+
+    let showPrintQr = false;
+</script>
+<Button on:click={() => (showPrintQr = true)}>
+    <DownloadButton alt="Download QR" />Download QR Code
+</Button>
+
+<Modal title="Print QR Code" bind:showModal={showPrintQr}>
+    <QrGenerator URL={trackingNumber} />
+    <p>Tracking Number: {trackingUrl}</p>
+    <a href={trackingUrl}>Go to Tracking Page</a>
+    <span id="bottom">
+        <Button submit><DownloadButton alt="Download QR"/> Download QR</Button>
+        <a href={trackingUrl}><Button type={ButtonType.Primary}>Go to Tracking Page</Button></a>
+    </span>
+</Modal>
+
+
+
