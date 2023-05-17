d4a0d3d72526bce945c44228f4bd6ed5de172140
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-18 07:31:15 +0800

    feat(client): add print button in modal

--- client/src/components/ui/qr/PrintQr.svelte
+++ client/src/components/ui/qr/PrintQr.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import DownloadButton from '../../icons/DocumentDownload.svelte'; 
+    import DownloadButton from '../../icons/DocumentDownload.svelte';
     import Button from '../Button.svelte';
     import Modal from '../Modal.svelte';
     import QrGenerator from './QrGenerator.svelte';
     import { ButtonType } from '../../types.ts';
+    
 
     export let trackingNumber: string;
-    const trackingUrl = `localhost:3000/track?id=${trackingNumber}`;
+    const trackingUrl = `http://localhost:3000/track?id=${trackingNumber}`;
 
     let showPrintQr = false;
 </script>
@@ -15,14 +16,23 @@
 </Button>
 
 <Modal title="Print QR Code" bind:showModal={showPrintQr}>
-    <QrGenerator URL={trackingNumber} />
-    <p>Tracking Number: {trackingUrl}</p>
-    <a href={trackingUrl}>Go to Tracking Page</a>
+    <center>
+        <QrGenerator URL={trackingNumber} />
+    </center>
+    <p>Tracking Number: {trackingNumber}</p>
     <span id="bottom">
         <Button submit><DownloadButton alt="Download QR"/> Download QR</Button>
+        <Button type={ButtonType.Primary} on:click={() => (window.print())}>Print</Button>
         <a href={trackingUrl}><Button type={ButtonType.Primary}>Go to Tracking Page</Button></a>
     </span>
 </Modal>
+
+<style>
+    #bottom {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
 
 
 
