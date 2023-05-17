4ee382b2c8a7feb95b21601c06e0d68bf5ff7ad3
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-18 06:33:17 +0800

    fix(client): qrcode dependency

--- client/src/components/ui/qr/QRGenerator.svelte
+++ client/src/components/ui/qr/QRGenerator.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-	
-	export let codeValue: string;
-	export let squareSize : number; 
-	
+
+export let codeValue: string;
+export let squareSize : number;
+
   let qrcode;
   
-	onMount(() => {
+onMount(() => {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js';
+      document.head.append(script);
 
-		let script = document.createElement('script');
-    script.src = "https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"
-    document.head.append(script);
-	
-		script.onload = function() {
-
-			qrcode = new QRCode("qrcode", {
-				text: codeValue,
+      script.onload = function() {
+          qrcode = new QRCode('qrcode', {
+              text: codeValue,
 		  	width: squareSize,
-        height: squareSize,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-			});		
-			
-		};				
-	  
-	});
+              height: squareSize,
+              colorDark: '#000000',
+              colorLight: '#ffffff',
+              correctLevel: QRCode.CorrectLevel.H,
+          });
+      };
+});
 </script>
 
 <style>
