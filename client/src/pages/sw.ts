46b0ade5c9088532b5750b0d39325c8451a9e38e
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-05-18 00:00:31 +0800

    feat(client): return sentinel response for uncached queries when offline

--- client/src/pages/sw.ts
+++ client/src/pages/sw.ts
@@ -118,8 +118,10 @@ async function handleFetch(req: Request): Promise<Response> {
     } catch (error) {
         assert(error instanceof TypeError);
         const maybeRes = await cache.match(req);
-        assert(maybeRes instanceof Response);
-        return maybeRes;
+        if (maybeRes instanceof Response) {
+            return maybeRes;
+        }
+        return new Response(null, { status: StatusCodes.SERVICE_UNAVAILABLE });
     }
 }
 
