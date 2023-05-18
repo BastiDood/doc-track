25d99c11768358e418327f35a0c675a7e071b8ab
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-18 14:08:20 +0800

    refactor(client): catch errors by enqueueing them to the toast

--- client/src/components/ui/qr/QrGenerator.svelte
+++ client/src/components/ui/qr/QrGenerator.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
-  import QRCode from 'qrcode';
+    import QRCode from 'qrcode';
 
-  export let URL: string;
+    import { assert } from '../../../assert.ts';
+    import { topToastMessage } from '../../../pages/dashboard/stores/ToastStore.ts';
 
+    export let url: string;
+
+    $: qrCode = QRCode.toDataURL(url).catch(err => {
+        assert(err instanceof Error);
+        topToastMessage.enqueue({ title: err.name, body: err.message });
+        return null;
+    });
 </script> 
 
-{#await QRCode.toDataURL(URL)}
-  <p>Generating QR Code...</p>
+{#await qrCode}
+    <p>Generating QR Code...</p>
 {:then dataUrl}
-  <img src={dataUrl} alt="QR Code" />
-{:catch error}
-  <p>Failed to generate QR Code: {error.message}</p>
+    {#if dataUrl === null}
+        <p>Failed to generate QR code.</p>
+    {:else}
+        <img src={dataUrl} alt="QR Code" />
+    {/if}
 {/await}
