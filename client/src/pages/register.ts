611ab708c7615436329636a4d8c8c4ad6379f3b5
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-19 13:39:01 +0800

    refactor(client): inline the subscription getter

--- client/src/pages/register.ts
+++ client/src/pages/register.ts
@@ -1,12 +1,3 @@
-export async function getSubscription(manager: PushManager): Promise<PushSubscription> {
-    const maybeSub = await manager.getSubscription();
-    if (maybeSub !== null) return maybeSub;
-    return manager.subscribe({
-        applicationServerKey: await Vapid.getVapidPublicKey(),
-        userVisibleOnly: true,
-    });
-}
-
 export function register() {
     return navigator.serviceWorker.register(new URL('sw.ts', import.meta.url), {
         type: 'module',
