a38380df03d130f1b949f4339b388fb72bcb5b30
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-20 02:12:42 +0800

    fmt(client): apply formatting refactor

--- client/src/components/ui/QRScanner.svelte
+++ client/src/components/ui/QRScanner.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import QrScanner from 'qr-scanner';
     import { createEventDispatcher, onDestroy, onMount } from 'svelte';
-    import { Events, ButtonType } from '../types';
-    import { assert } from '../../assert';
-    import { Document, DocumentSchema } from '../../../../model/src/document.ts';
+    import { type Document, DocumentSchema } from '~model/document.ts';
 
-    import { topToastMessage } from '../../pages/dashboard/stores/ToastStore';
     import Button from './Button.svelte';
+
+    import { assert } from '../../assert.ts';
+    import { Events, ButtonType } from '../types.ts';
+
+    import { topToastMessage } from '../../pages/dashboard/stores/ToastStore.ts';
 
     export let maybeId = null as Document['id'] | null;
 
@@ -14,7 +16,6 @@
     let camStart = false as boolean;
     let qrScanner = null as QrScanner | null;
     let videoElement = null as HTMLVideoElement | null;
-    let uploadElement = null as HTMLInputElement | null;
 
     async function startCamera() {
         camStart = true;
@@ -27,73 +28,69 @@
     }
 
     async function handleFileInput(this: HTMLInputElement) {
-        assert(this.files instanceof FileList);
-        const [first] = this.files;
+        assert(this.files !== null);
+        const [first, ...rest] = this.files;
+        assert(rest.length === 0);
         assert(typeof first !== 'undefined');
         try {
-            const scanData = await QrScanner.scanImage(first, { returnDetailedScanResult: true });
-            if (scanData.data === '') return;
-            maybeId = DocumentSchema.shape.id.parse(scanData.data.trim());
+            const { data } = await QrScanner.scanImage(first, { returnDetailedScanResult: true });
+            if (!data) return;
+            maybeId = DocumentSchema.shape.id.parse(data.trim());
             dispatch(Events.OnDocumentScan, maybeId);
         } catch (err) {
-            topToastMessage.enqueue({ title: 'Failed to Find QR code', body: JSON.stringify(err) });
+            topToastMessage.enqueue({ title: 'Failed to Find QR code.', body: JSON.stringify(err) });
         }
-        // eslint-disable-next-line require-atomic-updates
         this.value = '';
     }
 
     function setup() {
         assert(videoElement instanceof HTMLVideoElement);
-
         return new QrScanner(
             videoElement,
             result => {
-                if (result.data === '') return;
+                if (!result.data) return;
                 maybeId = DocumentSchema.shape.id.parse(result.data.trim());
                 stopCamera();
                 dispatch(Events.OnDocumentScan, maybeId);
-            }, {
+            },
+            {
                 highlightCodeOutline: true,
                 highlightScanRegion: true,
-                onDecodeError: (err) => {
+                maxScansPerSecond: 1,
+                returnDetailedScanResult: true,
+                onDecodeError(err) {
                     if (err === 'No QR code found') return;
                     topToastMessage.enqueue({ title: 'Failed to Find QR code', body: JSON.stringify(err) });
                 },
-                maxScansPerSecond: 1,
-                returnDetailedScanResult: true,
-            }
+            },
         );
     }
     
-    onMount(() => {
-        // Needs to be here so that the <video> tag is rendered before it is asserted
-        qrScanner = setup();
-    });
+    // Needs to be here so that the <video> tag is rendered before it is asserted
+    onMount(() => (qrScanner = setup()));
 
-    onDestroy(() => {
-        // Otherwise, when the window is closed - browser still thinks you are recording.
-        stopCamera();
-    });
+    // Otherwise, when the window is closed - browser still thinks you are recording.
+    onDestroy(stopCamera);
 </script>
 
 <video bind:this={videoElement}><track kind="captions"></video>
 {#await QrScanner.hasCamera()}
     Loading cameras...
-    {:then hasCamera}
-        {#if hasCamera}
+{:then hasCamera}
+    {#if hasCamera}
         <header>
             {#if camStart}
-                <Button type={ButtonType.Secondary} on:click={stopCamera}> End Capture </Button>
+                <Button type={ButtonType.Secondary} on:click={stopCamera}>End Capture</Button>
             {:else}
-                <Button on:click={startCamera}> Start Capture </Button>
+                <Button on:click={startCamera}>Start Capture</Button>
             {/if}
         </header>
-        {:else}
-            <p> No cameras available. </p>
-        {/if}
+    {:else}
+        <p>No cameras available.</p>
+    {/if}
 {/await}
 <header>
-    Upload a file with the QR code: <input bind:this={uploadElement} on:change={handleFileInput} type="file" id="file-selector" accept="image/*">
+    Upload a file with the QR code: <input type="file" accept="image/*" on:change={handleFileInput}>
 </header>
 {#if maybeId === null || maybeId === ''} 
     <p> No valid QR code detected. </p>
