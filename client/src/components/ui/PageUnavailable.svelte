1b57dfe97c3cff0a50bd473519a242bf91e27b47
Marc <mgc.macaraeg@gmail.com>
2023-05-26 00:37:49 +0800

    feat(client): add page unavailable component

---
+++ client/src/components/ui/PageUnavailable.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let err = null as Error | null;
+</script>
+
+<div>
+    <h2>Sorry, this page isn't available.</h2>
+    <p>The link is currently unavailable or you may be offline. <a href="/auth/login">Log back in?</a></p>
+    {#if err !== null}
+        <p>Technical Details: ({err.name}) {err.message}</p>
+    {/if}
+</div>
+
+<style>
+    div {
+        text-align: center;
+    }
+</style>
