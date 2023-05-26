ec84a881baba163a8b38747c36ecd4bc0f083d03
Arukuen <adsarmiento3@up.edu.ph>
2023-05-26 12:10:29 +0800

    fix(client): remove unnecessary context menu for user page

--- client/src/pages/dashboard/views/Users.svelte
+++ client/src/pages/dashboard/views/Users.svelte
@@ -8,26 +8,18 @@
     import PersonRowGlobal from '../../../components/ui/itemrow/PersonRowGlobal.svelte';
     import GlobalPermissions from '../../../components/ui/forms/permissions/GlobalPermissions.svelte';
     import Modal from '../../../components/ui/Modal.svelte';
-    import PersonContextGlobal from '../../../components/ui/contextdrawer/PersonContextGlobal.svelte';
     import PageUnavailable from '../../../components/ui/PageUnavailable.svelte';
 
     interface Context {
         id: User['id'],
         permissions: User['permission'],
-        context: boolean,
         showEdit: boolean,
     }
 
     let ctx = null as Context | null;
 
-    function openContext(id: User['id'], permissions: User['permission']) {
-        ctx = { id: id, permissions: permissions, context: true, showEdit: false };
-    }
-
-    function openEditGlobal(ctxcpy: Context) {
-        ctxcpy.context = false;
-        ctxcpy.showEdit = true;
-        ctx = ctxcpy;
+    function openEdit(id: User['id'], permissions: User['permission']) {
+        ctx = { id: id, permissions: permissions, showEdit: true };
     }
 
     function resetContext() {
@@ -53,7 +45,7 @@
             {picture}
             {permission}
             iconSize={IconSize.Large} 
-            on:overflowClick={openContext.bind(null, id, permission)} 
+            on:overflowClick={openEdit.bind(null, id, permission)} 
         />
     {:else}
         No users exist.
@@ -64,19 +56,13 @@
 {#if ctx === null} 
     <!-- Do not display anything! -->
 {:else if ctx.showEdit}
-    <Modal title="Edit Global Permissions" showModal>
+    <Modal title="Edit Global Permissions" showModal on:close={resetContext}>
         <GlobalPermissions 
             on:done={resetContext}
             id={ctx.id}
             permission={ctx.permissions}    
         />
     </Modal>
-{:else if ctx.context}
-    <PersonContextGlobal
-        show
-        on:close={resetContext}
-        on:editGlobalPermission={openEditGlobal.bind(null, ctx)}
-    />
 {/if}
 
 
