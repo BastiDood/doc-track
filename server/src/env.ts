239b61ed2f376d4aec8c002133b4a384481e73a5
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-26 22:11:21 +0800

    chore(server): add logs to environment variables

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,19 +3,19 @@ import { valid as isEmail } from 'email';
 import { ApplicationServerKeys } from 'webpush';
 
 const PORT = Deno.env.get('PORT');
-assert(PORT);
+assert(PORT, 'port not given');
 
 const GOOGLE_ID = Deno.env.get('GOOGLE_ID');
-assert(GOOGLE_ID);
+assert(GOOGLE_ID, 'no Google ID provided');
 
 const GOOGLE_SECRET = Deno.env.get('GOOGLE_SECRET');
-assert(GOOGLE_SECRET);
+assert(GOOGLE_SECRET, 'no Google secret provided');
 
 const OAUTH_REDIRECT = Deno.env.get('OAUTH_REDIRECT');
-assert(OAUTH_REDIRECT);
+assert(OAUTH_REDIRECT, 'no OAuth redirect set');
 
 const HOSTED_GSUITE_DOMAIN = Deno.env.get('HOSTED_GSUITE_DOMAIN');
-assert(HOSTED_GSUITE_DOMAIN);
+assert(HOSTED_GSUITE_DOMAIN, 'no hosted GSuite domain');
 
 const PG_HOSTNAME = Deno.env.get('PG_HOSTNAME') || '127.0.0.1';
 
@@ -30,10 +30,10 @@ const PG_USER = Deno.env.get('PG_USER') || 'postgres';
 const PG_POOL = Deno.env.get('PG_POOL');
 
 const VAPID_PUB_KEY = Deno.env.get('VAPID_PUB_KEY');
-assert(VAPID_PUB_KEY);
+assert(VAPID_PUB_KEY, 'no VAPID public key');
 
 const VAPID_PRV_KEY = Deno.env.get('VAPID_PRV_KEY');
-assert(VAPID_PRV_KEY);
+assert(VAPID_PRV_KEY, 'no VAPID private key');
 
 const VAPID_CREDENTIALS = await ApplicationServerKeys.fromJSON({
     publicKey: VAPID_PUB_KEY,
@@ -43,8 +43,8 @@ const VAPID_CREDENTIALS = await ApplicationServerKeys.fromJSON({
 const VAPID_RAW_PUB_KEY = await crypto.subtle.exportKey('raw', VAPID_CREDENTIALS.publicKey);
 
 const VAPID_EMAIL = Deno.env.get('VAPID_EMAIL');
-assert(VAPID_EMAIL);
-assert(isEmail(VAPID_EMAIL));
+assert(VAPID_EMAIL, 'no VAPID email preset');
+assert(isEmail(VAPID_EMAIL), 'provided VAPID email is invalid');
 
 export const env = {
     PORT: parseInt(PORT, 10),
