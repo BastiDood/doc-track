00817107c8d58c98d44e19728b9dd0188e37d29b
Justin Ruaya <jrruaya1@up.edu.ph>
2023-05-26 14:19:24 +0800

    feat(client): logo on side drawer

---
+++ client/src/components/logo/LogoSidedrawer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import './../icons/icon-styles.css';
+    export let alt: string;
+
+    const iconUrl = new URL('../../assets/icons/LogoMain.svg', import.meta.url);
+</script>
+
+<img id='logo' {alt} src="{iconUrl.pathname}" on:click on:keydown />
+
+
+<style>
+    #logo {
+        width: 60%;
+        height: 60%;
+        max-width: 512px;
+    }
+</style>
