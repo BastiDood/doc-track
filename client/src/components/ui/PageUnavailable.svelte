02132016f2a3f17c4e73264d86449f83f724c8a0
Arukuen <adsarmiento3@up.edu.ph>
2023-05-26 11:23:56 +0800

    feat(client): separate error display for invalid session

--- client/src/components/ui/PageUnavailable.svelte
+++ client/src/components/ui/PageUnavailable.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+  import { InvalidSession } from "../../api/error";
+
     export let err = null as Error | null;
 </script>
 
 <div>
     <h2>Sorry, this page isn't available.</h2>
-    <p>The link is currently unavailable or you may be offline. <a href="/auth/login">Log back in?</a></p>
     {#if err !== null}
-        <p>Technical Details: ({err.name}) {err.message}</p>
+        {#if err instanceof InvalidSession}
+            <p>Your session has expired. <a href="/auth/login">Log back in?</a></p>
+        {:else}
+            <p>Technical Details: ({err.name}) {err.message}</p>
+        {/if}
     {/if}
 </div>
 
