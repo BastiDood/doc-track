65a06a4258e17ad3f6637a7e49e42125ac3c4639
Marc <mgc.macaraeg@gmail.com>
2023-05-26 15:29:55 +0800

    fix(client): ensure that the Dossier is only loaded when an office is selected

--- client/src/pages/dashboard/views/Dossier.svelte
+++ client/src/pages/dashboard/views/Dossier.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { dashboardState } from '../../../stores/DashboardState.ts';
     import { topToastMessage } from '../../../stores/ToastStore.ts';
     import { assert } from '../../../assert.ts';
@@ -8,18 +8,22 @@
     import PageUnavailable from '../../../components/ui/PageUnavailable.svelte';
 
     $: ({ currentOffice } = $dashboardState);
-    $: dossier = Api.getDossier(currentOffice).catch(err => {
-        assert(err instanceof Error);
-        topToastMessage.enqueue({ title: err.name, body: err.message });
-        throw err;
-    });
+
+    function loadDossier(office: number) {
+        return Api.getDossier(office).catch(err => {
+            assert(err instanceof Error);
+            topToastMessage.enqueue({ title: err.name, body: err.message });
+            throw err;
+        });
+    }
+    
 </script>
 
 {#if currentOffice === null}
-    You must select an office before accessing the Dossier page.
+    <p>You must select an office before accessing the Dossier page.</p>
 {:else}
     <h1>Dossier</h1>
-    {#await dossier}
+    {#await loadDossier(currentOffice)}
         <p>Loading registered documents.</p>
     {:then reg}
         {#each reg as entry (entry.doc)}
