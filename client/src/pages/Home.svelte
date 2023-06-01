e60cfa0c0d79c844d418f62d487a9b8c02dea5c6
Marc <mgc.macaraeg@gmail.com>
2023-06-01 21:50:57 +0800

    feat(client): migrate `Home` page to flex centric layout

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -38,7 +38,7 @@
     {#await reg}
         Waiting for service worker...
     {:then}
-        <div class="middle-container" in:fade={{ duration: 1000 }}>
+        <article in:fade={{ duration: 1000 }}>
             <MainLogo alt='Logo' />
             <div id="doctrack">
                 {#each 'DOCTRACK' as char, i}
@@ -50,14 +50,14 @@
             <a href="/auth/login">
                 <Button type={ButtonType.Primary}><Google color={IconColor.White} alt="Log in with UP Mail" />Log in with University of the Philippines Mail</Button>
             </a>
-            <div class="search-container">
+            <section>
                 <TextInput placeholder="Enter tracking number here..." label="Tracking Number:" name="track-number" bind:value={trackingNumber} />
                 <Button type={ButtonType.Primary} on:click={() => (showScan = true)}><Camera color={IconColor.White} alt="Take/select an image." /></Button>
                 <a href={`/track?id=${trackingNumber}`}>
                     <Button type={ButtonType.Primary}><Search color={IconColor.White} alt="Search specified tracking number." /></Button>
                 </a>
-            </div>
-        </div>
+            </section>
+        </article>
     {:catch err}
         <PageUnavailable {err} />
     {/await}
@@ -78,6 +78,16 @@
         width: 100%;
     }
 
+    article {
+        padding: var(--spacing-large);
+        border: var(--spacing-tiny) solid var(--primary-color);
+        background-color: var(--login-bg);
+        border-radius: var(--border-radius);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
     a {
         text-decoration: none;
     }
@@ -89,25 +99,19 @@
         font-weight: 400;
     }
 
-    .middle-container {
-        text-align: center;
-        padding: var(--spacing-large);
-        border: var(--spacing-tiny) solid var(--primary-color);
-        background-color: var(--login-bg);
-        border-radius: var(--border-radius);
-    }
-
-    .search-container {
-        padding: var(--spacing-small);
-        margin: 0 auto;
-    }
-
     #doctrack {
         color: var(--primary-color);
         display: flex;
         font-size: 2rem;
         font-weight: 700;
         justify-content: center;
-        gap: 0.75rem;
+        column-gap: 0.75rem;
     }
+
+    section {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
 </style>
