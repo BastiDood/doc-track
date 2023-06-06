0636379218e1b458d41a5c47d1529b89e4c19e06
Justin Ruaya <jrruaya1@up.edu.ph>
2023-06-06 17:10:15 +0800

    chore(client): remove unnecessary alt on DoctrackWink

--- client/src/components/doctrack/DoctrackWink.svelte
+++ client/src/components/doctrack/DoctrackWink.svelte
@@ -5,12 +5,12 @@
     const iconUrl_winked = new URL('../../assets/logo/doctrack-wink.svg', import.meta.url);
 </script>
 
-<article on:mouseenter={ () => { isWinked = true; } } on:mouseleave={ () => { isWinked = false; } }>
+<span on:mouseenter={ () => { isWinked = true; } } on:mouseleave={ () => { isWinked = false; } }>
     <img alt="Doctrack logo" src={isWinked ? iconUrl_winked.pathname : iconUrl.pathname} on:click on:keydown />
-</article>
+</span>
 
 <style>
-    article {
+    span {
         cursor: pointer;
     }
 </style>
