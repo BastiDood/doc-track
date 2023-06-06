af525691e53ca289da06db269f9a09e082883337
Justin Ruaya <jrruaya1@up.edu.ph>
2023-06-06 21:00:15 +0800

    minor(client): container animation set to 100ms

--- client/src/components/ui/Container.svelte
+++ client/src/components/ui/Container.svelte
@@ -7,7 +7,7 @@
     export let ty: ContainerType;
 </script>
 
-<section class={ty} in:fade={{ duration: 150, easing: quadInOut }}>
+<section class={ty} in:fade={{ duration: 100, easing: quadInOut }}>
     <slot></slot>
 </section>
 
